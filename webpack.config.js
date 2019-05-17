7d39d9fdcffd61bb73ccd14fb7072e59ae6c7edc
LOLDeveloper <50070488+LOLdevelopr@users.noreply.github.com>
2019-05-18 00:33:46 +0530

    fix : resolve error and refractor

--- webpack.config.js
+++ webpack.config.js
@@ -8,97 +8,92 @@ const
 	development = !['production', 'ci'].includes(process.env.NODE_ENV)
 
 module.exports = {
-		devtool: 'source-map',
-		entry: [
-			path.join(__dirname, process.env.TEST_RUN ? 'client/test/index' : 'client/main')
-		].filter(x => x),
-		output: {
-			path: path.join(__dirname, 'dist'),
-			filename: 'cadence.[hash].js',
-			publicPath: '/'
-		},
-		plugins: [
-			!development && new webpack.DefinePlugin({
-				'process.env': {
-					NODE_ENV: '"production"'
-				}
-			}),
-			new MiniCssExtractPlugin({
-				filename: development ? 'cadence.css' : 'cadence.[hash].css',
-			}),
-			new HtmlWebpackPlugin({
-				title: 'Cadence',
-				filename: 'index.html',
-				favicon: 'favicon.ico',
-				inject: false,
-				template: require('html-webpack-template'),
-				lang: 'en-US',
-				scripts: (process.env.CADENCE_EXTERNAL_SCRIPTS || '').split(',').filter(x => x)
-			})
-		].filter(x => x),
-		module: {
-			rules: [{
-					test: /\.css$/,
-					use: [
-						{
-							test: /\.css$/,
-							use: [
-							  {
-								loader: MiniCssExtractPlugin.loader,
-								options: {
-								  hmr: development,
-								},
-							  },
-							  'css-loader',
-							],
-						  },
-					{
-						test: /\.vue?$/,
-						loader: 'vue-loader',
-						options: {
-							loaders: {
-								stylus: ExtractTextPlugin.extract({
-									use: extractStylus,
-									fallback: 'vue-style-loader'
-								}),
-							}
-						}
-					},
-					{
-						test: /\.svg$/,
-						use: [{
-							loader: 'html-loader',
-							options: {
-								minimize: true
-							}
-						}]
-					},
-					{
-						test: /\.(png|jpg|gif)$/,
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]?[hash]'
-						}
-					},
-					{
-						test: /\.styl$/,
-						use: ExtractTextPlugin.extract({
-							use: extractStylus,
-							fallback: 'raw-loader'
-						}),
-						include: path.join(__dirname, 'src')
-					}]
-			},
-			resolve: {
-				extensions: ['.js', '.vue']
-			},
-			resolve: {
-				alias: {
-					'vue$': 'vue/dist/vue.esm.js'
-				}
-			},
-			devServer: {
-				historyApiFallback: true,
-				overlay: true
+	devtool: 'source-map',
+	entry: [
+		path.join(__dirname, process.env.TEST_RUN ? 'client/test/index' : 'client/main')
+	].filter(x => x),
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'cadence.[hash].js',
+		publicPath: '/'
+	},
+	plugins: [
+		!development && new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
 			}
+		}),
+		new MiniCssExtractPlugin({
+			filename: development ? 'cadence.css' : 'cadence.[hash].css',
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Cadence',
+			filename: 'index.html',
+			favicon: 'favicon.ico',
+			inject: false,
+			template: require('html-webpack-template'),
+			lang: 'en-US',
+			scripts: (process.env.CADENCE_EXTERNAL_SCRIPTS || '').split(',').filter(x => x)
+		})
+	].filter(x => x),
+	module: {
+		rules: [{
+				test: /\.css$/,
+				use: [{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: development,
+						},
+					},
+					'css-loader',
+				],
+			},
+			{
+				test: /\.vue?$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						stylus: ExtractTextPlugin.extract({
+							use: extractStylus,
+							fallback: 'vue-style-loader'
+						}),
+					}
+				}
+			},
+			{
+				test: /\.svg$/,
+				use: [{
+					loader: 'html-loader',
+					options: {
+						minimize: true
+					}
+				}]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]?[hash]'
+				}
+			},
+			{
+				test: /\.styl$/,
+				use: ExtractTextPlugin.extract({
+					use: extractStylus,
+					fallback: 'raw-loader'
+				}),
+				include: path.join(__dirname, 'src')
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.vue'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
 		}
+	},
+	devServer: {
+		historyApiFallback: true,
+		overlay: true
+	}
+}
