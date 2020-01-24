49931c8751b47cf860ab433eeec2ee3379b64187
Justin Muller <just@uber.com>
2020-01-24 12:29:57 -0800

    fixing lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: [
     'jest',
+    'prettier',
     'vue',
   ],
   rules: {
--- client/helpers/json-try-parse.js
+++ client/helpers/json-try-parse.js
@@ -1,7 +1,10 @@
-function jsonTryParse() {
+function jsonTryParse(...args) {
   try {
-    return JSON.parse.apply(this, arguments);
-  } catch (e) { }
+    return JSON.parse.apply(this, args);
+  } catch (e) {
+    console.warn('JSON.parse failed for args:', args);
+  }
+  return undefined;
 }
 
 export default jsonTryParse;
