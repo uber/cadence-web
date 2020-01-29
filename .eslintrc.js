004c4819b9675b4939c73d26674d036cfe1eb5af
Justin Muller <just@uber.com>
2020-01-29 10:35:36 -0800

    removing commented code

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,17 +50,5 @@ module.exports = {
         rangeEnd: Infinity,
       },
     ],
-    // see https://stackoverflow.com/questions/44939304/eslint-should-be-listed-in-the-projects-dependencies-not-devdependencies#answer-55863857
-    // "import/no-extraneous-dependencies": [
-    //   "error",
-    //   {
-    //     "devDependencies": [
-    //       "client/test/index.js",
-    //       "client/test/scenario.js",
-    //       "**/*.test.js",
-    //       "**/*.spec.js",
-    //     ],
-    //   },
-    // ],
   },
 };
