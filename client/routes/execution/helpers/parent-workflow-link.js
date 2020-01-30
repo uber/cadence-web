99c70fb4a47d3a935e6f6d98cdab2b369effb8e7
Justin Muller <just@uber.com>
2020-01-29 17:04:15 -0800

    running eslint to fix any issues after merge

--- client/routes/execution/helpers/parent-workflow-link.js
+++ client/routes/execution/helpers/parent-workflow-link.js
@@ -1,16 +1,16 @@
 export default function(wfStartDetails) {
-    if (wfStartDetails && wfStartDetails.parentWorkflowExecution) {
-        return {
-            to: {
-                name: 'execution/summary',
-                params: {
-                    domain: wfStartDetails.parentWorkflowDomain,
-                    workflowId: wfStartDetails.parentWorkflowExecution.workflowId,
-                    runId: wfStartDetails.parentWorkflowExecution.runId,
-                },
-            },
-            text: wfStartDetails.parentWorkflowExecution.workflowId,
-        };
-    }
-    return null;
+  if (wfStartDetails && wfStartDetails.parentWorkflowExecution) {
+    return {
+      to: {
+        name: 'execution/summary',
+        params: {
+          domain: wfStartDetails.parentWorkflowDomain,
+          workflowId: wfStartDetails.parentWorkflowExecution.workflowId,
+          runId: wfStartDetails.parentWorkflowExecution.runId,
+        },
+      },
+      text: wfStartDetails.parentWorkflowExecution.workflowId,
+    };
+  }
+  return null;
 }
