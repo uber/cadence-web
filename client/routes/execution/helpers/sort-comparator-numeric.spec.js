e75b79ab872aba582b14f4c5729dfff5c484e766
Justin Muller <just@uber.com>
2020-02-15 13:43:11 -0800

    adding test for list sorting

---
+++ client/routes/execution/helpers/sort-comparator-numeric.spec.js
@@ -0,0 +1,58 @@
+import sortComparatorNumeric from './sort-comparator-numeric';
+
+const LIST_ASCENDING_ORDER = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+];
+
+const LIST_DESCENDING_ORDER = [
+  {
+    id: 3,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 1,
+  },
+];
+
+describe('sortComparatorNumeric', () => {
+  const initList = () => {
+    return [
+      {
+        id: 3,
+      },
+      {
+        id: 1,
+      },
+      {
+        id: 2,
+      },
+    ];
+  };
+
+  describe('when key = id', () => {
+    describe('and ascending = true', () => {
+      it('should sort the list into ascending order.', () => {
+        const list = initList();
+        list.sort(sortComparatorNumeric('id', true));
+        expect(list).toEqual(LIST_ASCENDING_ORDER);
+      });
+    });
+    describe('and ascending = false', () => {
+      it('should sort the list into descending order.', () => {
+        const list = initList();
+        list.sort(sortComparatorNumeric('id', false));
+        expect(list).toEqual(LIST_DESCENDING_ORDER);
+      });
+    });
+  });
+});
