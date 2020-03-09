8a7e45ee13997b4a82acd808e853cf79a521bdac
Justin Muller <just@uber.com>
2020-03-09 09:20:25 -0700

    removing old date range styles. adjusting styling

--- client/components/date-range-picker.vue
+++ client/components/date-range-picker.vue
@@ -165,17 +165,17 @@ export default {
 .date-range-picker
   display: flex;
 
-  bkgnd-color = #f2f2f4
-  cell-size = 36px
-  header-height = 46px
-  brdr = 1px solid uber-white-40
-
   .relative-range {
     width: 160px;
   }
+
   .custom-range {
-    padding-left: 10px;
+    padding-left: 16px;
     position: relative;
+  }
+
+  .mx-datepicker-range {
+    width: auto;
 
     input {
       border: 1px solid #e5e5e4;
@@ -187,74 +187,9 @@ export default {
       width: 325px;
     }
   }
+
   input.form-control {
     width: 170px;
   }
-  .ayou-date-range
-    position absolute
-    box-shadow 1px 1px 2px rgba(0,0,0,0.2)
-    top calc(100% + 26px)
-    left 10px
-    width 254px
-    &::before
-      position absolute
-      content ' '
-      background-color bkgnd-color
-      width 32px
-      height 32px
-      border brdr
-      left calc(50% - 16px)
-      top -16px
-      transform rotate(45deg)
-
-    .ayou-calendar, .month-year .month-button
-      background-color bkgnd-color
-    .month-year
-      height header-height
-      display flex
-      border brdr
-      border-bottom none
-      .month-button i
-        border-color black
-      > span
-        flex 1 1 auto
-        z-index 5
-        background-color bkgnd-color
-      span
-        display inline-block
-        line-height header-height
-        vertical-align center
-        font-size 16px
-        font-weight 500
-
-    .week-days span, .days span, .days div
-      font-size 10px
-    .days
-      background-color white
-      border brdr
-      border-bottom none
-      div.solar
-        width 100%
-        height 100%
-        line-height cell-size
-      .ayou-day-cell
-        padding 0
-        width cell-size
-        height cell-size
-        line-height cell-size
-        border-bottom brdr
-        border-right brdr
-        &.in-range
-          range-color = alpha(uber-blue, 85%)
-          border-bottom-color range-color
-          border-right-color range-color
-          .solar
-            border-radius 0
-            background-color range-color
-          &.start-day, &.end-day
-            font-weight 500
-            background-color uber-blue
-        &:nth-child(7n)
-          border-right none
 
 </style>
