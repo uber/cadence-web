9b659effac2a92762fb02600b5b0244f1cfabea9
Justin Muller <just@uber.com>
2020-03-10 14:35:52 -0700

    testing isDayDisabled

--- client/components/date-range-picker/helpers.spec.js
+++ client/components/date-range-picker/helpers.spec.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
 import {
   getMaxStartDate,
   getTimePanelLabel,
+  isDayDisabled,
 } from './helpers';
 
 describe('DateRangePicker helpers', () => {
@@ -67,6 +69,58 @@ describe('DateRangePicker helpers', () => {
   });
 
   describe('isDayDisabled', () => {
-    // TODO
+    describe('When moment is set to March 10th 2020', () => {
+      beforeEach(() => {
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(2020, 2, 10).getTime());
+      });
+
+      describe('and maxStartDate = March 8th 2020', () => {
+        let maxStartDate;
+
+        beforeEach(() => {
+          maxStartDate = moment(new Date(2020, 2, 8));
+        });
+
+        describe('and date = March 10th 2020', () => {
+          it('should return false.', () => {
+            const date = new Date(2020, 2, 10);
+            const output = isDayDisabled(maxStartDate)(date);
+            expect(output).toEqual(false);
+          });
+        });
+
+        describe('and date = March 9th 2020', () => {
+          it('should return false.', () => {
+            const date = new Date(2020, 2, 9);
+            const output = isDayDisabled(maxStartDate)(date);
+            expect(output).toEqual(false);
+          });
+        });
+
+        describe('and date = March 8th 2020', () => {
+          it('should return false.', () => {
+            const date = new Date(2020, 2, 8);
+            const output = isDayDisabled(maxStartDate)(date);
+            expect(output).toEqual(false);
+          });
+        });
+
+        describe('and date = March 7th 2020', () => {
+          it('should return true.', () => {
+            const date = new Date(2020, 2, 7);
+            const output = isDayDisabled(maxStartDate)(date);
+            expect(output).toEqual(true);
+          });
+        });
+
+        describe('and date = March 11th 2020', () => {
+          it('should return true.', () => {
+            const date = new Date(2020, 2, 11);
+            const output = isDayDisabled(maxStartDate)(date);
+            expect(output).toEqual(true);
+          });
+        });
+      });
+    });
   });
 });
