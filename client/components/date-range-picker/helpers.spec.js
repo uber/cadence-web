acc5f34d8ed147f02e26ae1eb35e62bf45afb261
Justin Muller <just@uber.com>
2020-03-10 13:51:58 -0700

    Adding tests and mocking moment

--- client/components/date-range-picker/helpers.spec.js
+++ client/components/date-range-picker/helpers.spec.js
@@ -1,25 +1,36 @@
-import { getTimePanelLabel } from './helpers';
+import {
+  getMaxStartDate,
+  getTimePanelLabel,
+} from './helpers';
 
 describe('DateRangePicker helpers', () => {
   describe('getMaxStartDate', () => {
     describe('When moment is set to March 10th 2020', () => {
-
+      beforeEach(() => {
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(2020, 2, 10).getTime());
+      });
 
       describe('and maxDays = 1', () => {
         it('should return date March 9th 2020.', () => {
-          // TODO
+          const maxDays = 1;
+          const output = getMaxStartDate(maxDays);
+          expect(output.toISOString()).toEqual('2020-03-09T07:00:00.000Z');
         });
       });
 
       describe('and maxDays = 3', () => {
         it('should return date March 7th 2020.', () => {
-          // TODO
+          const maxDays = 3;
+          const output = getMaxStartDate(maxDays);
+          expect(output.toISOString()).toEqual('2020-03-07T08:00:00.000Z');
         });
       });
 
       describe('and maxDays = 30', () => {
-        it('should return date Feburary 10th 2020.', () => {
-          // TODO
+        it('should return date Feburary 9th 2020.', () => {
+          const maxDays = 30;
+          const output = getMaxStartDate(maxDays);
+          expect(output.toISOString()).toEqual('2020-02-09T08:00:00.000Z');
         });
       });
     });
