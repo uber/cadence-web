72f0b992b23fa18067fca3a1d9a9fa8874411c17
Justin Muller <just@uber.com>
2020-03-11 14:54:26 -0700

    refactoring based on user feedback. needs more work

--- client/components/date-range-picker/constants.js
+++ client/components/date-range-picker/constants.js
@@ -11,12 +11,9 @@ export const ALLOWED_PERIOD_TYPES = [
   'months',
 ];
 
-export const DATETIME_FORMAT = 'MM/DD/YYYY HH:mm:ss';
+export const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 export const RANGE_OPTIONS = [
-  { text: 'Last 1 second', value: 'last-1-second', daysAgo: 0.00001 },
-  { text: 'Last 10 seconds', value: 'last-10-seconds', daysAgo: 0.0001 },
-  { text: 'Last 60 seconds', value: 'last-60-seconds', daysAgo: 0.0007 },
   { text: 'Last 10 minutes', value: 'last-10-minutes', daysAgo: 0.007 },
   { text: 'Last 60 minutes', value: 'last-60-minutes', daysAgo: 0.041 },
   { text: 'Last 3 hours', value: 'last-3-hours', daysAgo: 0.125 },
--- client/components/date-range-picker/index.vue
+++ client/components/date-range-picker/index.vue
@@ -7,7 +7,7 @@
       :clearable="false"
       :disabled-date="isDayDisabled"
       :showTimePanel="showTimePanel"
-      :shortcuts="shortcuts"
+
       :open.sync="open"
       @change="onDateRangeChange"
     >
@@ -18,6 +18,38 @@
           type="text"
           :value="rangeDisplayText"
         />
+      </template>
+      <template v-slot:sidebar>
+        <div class="sidebar-column sidebar-column-shortcuts">
+          <h5>Quick ranges</h5>
+          <button
+            class="mx-btn mx-btn-text mx-btn-shortcut"
+            type="button"
+            v-for="shortcut in shortcuts"
+            :key="shortcut.value"
+            @click="onShortcutClick(shortcut)"
+          >{{ shortcut.text }}
+          </button>
+        </div>
+        <div class="sidebar-column sidebar-column-custom-range">
+          <h5>Custom range</h5>
+          <div>
+            <label for="custom-range-from">From:</label>
+            <input id="custom-range-from" value="2020-03-10 14:09:00" />
+          </div>
+          <div>
+            <label for="custom-range-to">to:</label>
+            <input id="custom-range-to" value="2020-03-10 14:09:00" />
+          </div>
+          <div>
+            <label for="custom-range-filtered-by">Filtered by:</label>
+            <!-- open workflow it is by startTime, for closed workflow is is by closeTime -->
+            <input id="custom-range-filtered-by" disabled value="StartTime" />
+          </div>
+          <div>
+            <button class="sidebar-button">Apply</button>
+          </div>
+        </div>
       </template>
       <template v-slot:footer>
         <button class="mx-btn mx-btn-text" @click="onClickTimePanelLabel">
@@ -66,8 +98,8 @@ export default {
     },
   },
   methods: {
-    onShortcutClick(range) {
-      this.$emit('change', range.value);
+    onShortcutClick(shortcut) {
+      this.$emit('change', shortcut.value);
       this.open = false;
     },
     onDateRangeChange(range) {
@@ -95,7 +127,10 @@ export default {
 </script>
 
 <style lang="stylus">
-sidebarWidth = 125px;
+sidebarColumnCustomRangeWidth = 185px;
+sidebarColumnShortcutsWidth = 145px;
+sidebarColumnPadding = 12px;
+sidebarWidth = sidebarColumnShortcutsWidth + sidebarColumnCustomRangeWidth;
 
 .mx-datepicker-popup {
   td {
@@ -103,11 +138,56 @@ sidebarWidth = 125px;
   }
 
   .mx-datepicker-sidebar {
+    padding: 6px 0;
     width: sidebarWidth;
 
     &+ .mx-datepicker-content {
       margin-left: sidebarWidth;
     }
+  }
+
+  .sidebar-column {
+    float: left;
+    height: 100%;
+    padding: 0 sidebarColumnPadding;
+
+    &+ .sidebar-column {
+      border-left: 1px solid #e8e8e8;
+    }
+
+    label {
+      display: block;
+      font-weight: 500;
+    }
+
+    button {
+      padding: 0;
+    }
+
+    input {
+      margin: 0 0 8px 0;
+      padding: 8px 10px;
+      width: 160px;
+    }
+  }
+
+  .sidebar-column-shortcuts {
+    width: sidebarColumnShortcutsWidth;
+  }
+
+  .sidebar-column-custom-range {
+    width: sidebarColumnCustomRangeWidth;
+  }
+
+  button.sidebar-button {
+    background-color: #11939a;
+    border: none;
+    color: #fff;
+    display: inline-block;
+    float: right;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 8px 12px;
   }
 }
 
