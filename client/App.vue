92c5611e74ed1d75db3df1d7b8cf3f4ce29e427b
Justin Muller <just@uber.com>
2020-03-17 16:41:26 -0700

    fixing eslint. removing unused function

--- client/App.vue
+++ client/App.vue
@@ -23,6 +23,23 @@ export default {
     clearTimeout(this.notification.timeout);
   },
   methods: {
+    globalClick(e) {
+      // Code required for mocha tests to run correctly without infinite looping.
+      if (e.target.tagName === 'A') {
+        const href = e.target.getAttribute('href');
+
+        if (
+          href &&
+          href.startsWith('/') &&
+          !e.target.getAttribute('download') &&
+          !e.target.getAttribute('target')
+        ) {
+          e.preventDefault();
+          e.stopPropagation();
+          this.$router.push(href);
+        }
+      }
+    },
     onNotification({ message, type = NOTIFICATION_TYPE_SUCCESS }) {
       this.notification.message = message;
       this.notification.type = type;
@@ -53,7 +70,7 @@ export default {
 </script>
 
 <template>
-  <main>
+  <main @click="globalClick">
     <NotificationBar
       :message="notification.message"
       :onClose="onNotificationClose"
