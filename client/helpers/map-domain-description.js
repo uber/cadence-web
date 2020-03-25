265332bb08f3a3653fd072c8b9589ae65bc6802f
Justin Muller <just@uber.com>
2020-03-25 15:56:28 -0700

    lint fixes

--- client/helpers/map-domain-description.js
+++ client/helpers/map-domain-description.js
@@ -6,37 +6,35 @@ export default function(domain) {
       workflowExecutionRetentionPeriodInDays,
       visibilityArchivalStatus,
     } = {},
-    domainInfo: {
-      description,
-      ownerEmail,
-    } = {},
+    domainInfo: { description, ownerEmail } = {},
     failoverVersion,
     isGlobalDomain,
-    replicationConfiguration: {
-      activeClusterName,
-      clusters = [],
-    } = {},
+    replicationConfiguration: { activeClusterName, clusters = [] } = {},
   } = domain || {};
 
   return {
     description: description || 'No description available',
     owner: ownerEmail || 'Unknown',
     'Global?': isGlobalDomain ? 'Yes' : 'No',
-    'Retention Period': workflowExecutionRetentionPeriodInDays ?
-      `${workflowExecutionRetentionPeriodInDays} days`
+    'Retention Period': workflowExecutionRetentionPeriodInDays
+      ? `${workflowExecutionRetentionPeriodInDays} days`
       : 'Unknown',
     'Emit Metrics': emitMetric ? 'Yes' : 'No',
-    'History Archival': historyArchivalStatus === 'ENABLED' ? 'Enabled' : 'Disabled',
-    'Visibility Archival': visibilityArchivalStatus === 'ENABLED' ? 'Enabled' : 'Disabled',
-    ...(failoverVersion !== undefined && { 'Failover Version': failoverVersion }),
-    clusters: clusters.length ?
-      clusters
-        .map(({ clusterName }) =>
-          clusterName === activeClusterName
-            ? `${clusterName} (active)`
-            : clusterName
-        )
-        .join(', ')
+    'History Archival':
+      historyArchivalStatus === 'ENABLED' ? 'Enabled' : 'Disabled',
+    'Visibility Archival':
+      visibilityArchivalStatus === 'ENABLED' ? 'Enabled' : 'Disabled',
+    ...(failoverVersion !== undefined && {
+      'Failover Version': failoverVersion,
+    }),
+    clusters: clusters.length
+      ? clusters
+          .map(({ clusterName }) =>
+            clusterName === activeClusterName
+              ? `${clusterName} (active)`
+              : clusterName
+          )
+          .join(', ')
       : 'Unknown',
   };
 }
