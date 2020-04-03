06ad2f57a19a3d12263c5c27c9a03c1dfb2f1d66
Justin <58960161+just-at-uber@users.noreply.github.com>
2020-04-03 15:20:29 -0700

    grid (#123)
    * grid
    
    * renaming component to flex grid
    
    * renaming flex grid column to flex grid item

--- client/components/index.js
+++ client/components/index.js
@@ -6,6 +6,8 @@ export { default as DetailList } from './detail-list';
 export { default as DateRangePicker } from './date-range-picker';
 export { default as DomainNavigation } from './domain-navigation';
 export { default as ErrorMessage } from './error-message';
+export { default as FlexGrid } from './flex-grid';
+export { default as FlexGridItem } from './flex-grid-item';
 export { default as NavigationBar } from './navigation-bar';
 export { default as NavigationLink } from './navigation-link';
 export { default as NotificationBar } from './notification-bar';
