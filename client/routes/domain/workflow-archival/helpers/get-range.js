b10a5aaa2d03781333d16b427c7f79932373296c
Justin Muller <just@uber.com>
2020-04-02 17:01:21 -0700

    adding tests for get range.

--- client/routes/domain/workflow-archival/helpers/get-range.js
+++ client/routes/domain/workflow-archival/helpers/get-range.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-export default ({ endTime, range = 'last-30-days', startTime }) =>
+export default ({ endTime, range = 'last-30-days', startTime } = {}) =>
   startTime && endTime ?
     {
       endTime: moment(endTime),
--- client/routes/domain/workflow-archival/helpers/get-range.spec.js
+++ client/routes/domain/workflow-archival/helpers/get-range.spec.js
@@ -1,3 +1,40 @@
+import moment from 'moment';
+import getRange from './get-range';
+
 describe('getRange', () => {
-  // TODO
+  describe('When nothing is passed', () => {
+    it('should return "last-30-days".', () => {
+      const output = getRange();
+      expect(output).toEqual('last-30-days');
+    });
+  });
+
+  describe('When range = "last-10-days"', () => {
+    it('should return "last-10-days".', () => {
+      const range = 'last-10-days';
+      const output = getRange({ range });
+      expect(output).toEqual('last-10-days');
+    });
+  });
+
+  describe('When startTime = "2020-03-01T00:00:00Z" and endTime = "2020-03-30T00:00:00Z"', () => {
+    let startTime;
+    let endTime;
+
+    beforeEach(() => {
+      startTime = '2020-03-01T00:00:00Z';
+      endTime = '2020-03-30T00:00:00Z';
+    });
+
+
+    it('should return startTime = moment("2020-03-01T00:00:00Z").', () => {
+      const output = getRange({ endTime, startTime });
+      expect(output.startTime).toEqual(moment("2020-03-01T00:00:00Z"));
+    });
+
+    it('should return endTime = moment("2020-03-30T00:00:00Z").', () => {
+      const output = getRange({ endTime, startTime });
+      expect(output.endTime).toEqual(moment("2020-03-30T00:00:00Z"));
+    });
+  });
 });
