a8f574bed2100c80d5acdf5a815cdfd961989a34
Justin Muller <just@uber.com>
2020-04-03 13:47:07 -0700

    grid

---
+++ client/components/grid.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="stylus">
+.grid {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
