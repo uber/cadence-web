5f04c347caea6c37f8c11ab43395dc8ef90203cd
Justin <58960161+just-at-uber@users.noreply.github.com>
2020-04-03 15:11:56 -0700

    error-message (#122)
    * error-message
    
    * end file return

---
+++ client/components/error-message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error-message-container" v-if="error">
+    <span class="error">
+      {{ error }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error-message',
+  props: ['error'],
+};
+</script>
+
+<style lang="stylus">
+.error-message-container {
+  padding: 70px 0;
+}
+</style>
--- client/components/index.js
+++ client/components/index.js
@@ -5,6 +5,7 @@ export { default as DataViewer } from './data-viewer';
 export { default as DetailList } from './detail-list';
 export { default as DateRangePicker } from './date-range-picker';
 export { default as DomainNavigation } from './domain-navigation';
+export { default as ErrorMessage } from './error-message';
 export { default as NavigationBar } from './navigation-bar';
 export { default as NavigationLink } from './navigation-link';
 export { default as NotificationBar } from './notification-bar';
