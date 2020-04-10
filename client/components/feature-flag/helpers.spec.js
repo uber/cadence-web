8ed7ab6f50b8fd1dfd7434eda9f1aec0625cad7f
Justin Muller <just@uber.com>
2020-04-10 15:05:34 -0700

    PR comments

--- client/components/feature-flag/helpers.spec.js
+++ client/components/feature-flag/helpers.spec.js
@@ -2,43 +2,35 @@ import { isEnabled, mapFlagsToHash } from './helpers';
 
 describe('feature-flag helpers', () => {
   describe('isEnabled', () => {
-    describe('When passed name = "workflow-terminate" and flagHash = {}', () => {
-      it('should return false.', () => {
-        const name = 'workflow-terminate';
-        const flagHash = {};
-        const output = isEnabled({ flagHash, name });
-        expect(output).toEqual(false);
-      });
+    it('When passed name = "workflow-terminate" and flagHash = {} it should return false.', () => {
+      const name = 'workflow-terminate';
+      const flagHash = {};
+      const output = isEnabled({ flagHash, name });
+      expect(output).toEqual(false);
     });
 
-    describe('When passed name = "workflow-terminate" and flagHash = { "workflow-terminate": true }', () => {
-      it('should return true.', () => {
-        const name = 'workflow-terminate';
-        const flagHash = { 'workflow-terminate': true };
-        const output = isEnabled({ flagHash, name });
-        expect(output).toEqual(true);
-      });
+    it('When passed name = "workflow-terminate" and flagHash = { "workflow-terminate": true } it should return true.', () => {
+      const name = 'workflow-terminate';
+      const flagHash = { 'workflow-terminate': true };
+      const output = isEnabled({ flagHash, name });
+      expect(output).toEqual(true);
     });
 
-    describe('When passed name = "workflow-terminate" and flagHash = { "workflow-terminate": false }', () => {
-      it('should return false.', () => {
-        const name = 'workflow-terminate';
-        const flagHash = { 'workflow-terminate': false };
-        const output = isEnabled({ flagHash, name });
-        expect(output).toEqual(false);
-      });
+    it('When passed name = "workflow-terminate" and flagHash = { "workflow-terminate": false } it should return false.', () => {
+      const name = 'workflow-terminate';
+      const flagHash = { 'workflow-terminate': false };
+      const output = isEnabled({ flagHash, name });
+      expect(output).toEqual(false);
     });
   });
 
   describe('mapFlagsToHash', () => {
-    describe('When passed flagArray = [{ key: "workflow-terminate", value: true }]', () => {
-      it('should return { "workflow-terminate": true }.', () => {
-        const flagArray = [
-          { key: 'workflow-terminate', value: true }
-        ];
-        const output = mapFlagsToHash(flagArray);
-        expect(output).toEqual({ 'workflow-terminate': true });
-      });
+    it('When passed flagArray = [{ key: "workflow-terminate", value: true }] it should return { "workflow-terminate": true }.', () => {
+      const flagArray = [
+        { key: 'workflow-terminate', value: true }
+      ];
+      const output = mapFlagsToHash(flagArray);
+      expect(output).toEqual({ 'workflow-terminate': true });
     });
   });
 });
