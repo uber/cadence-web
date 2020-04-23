54220d5ba8d3083b156872fe641dcba36ef8d83d
Justin Muller <just@uber.com>
2020-04-23 10:33:13 -0700

    adding vuepress for blog

---
+++ news/README.md
@@ -0,0 +1,13 @@
+# news
+
+> 
+
+## Development
+
+```bash
+yarn dev
+yarn build
+```
+
+For more details, please head VuePress's [documentation](https://v1.vuepress.vuejs.org/).
+
---
+++ news/blog/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: 'VuePress Blog Example',
+  description: 'This is a blog example built by VuePress',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/ulivz',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/_ulivz',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  },
+}
