f0b766bdb068718dabb782c06f433f92ccd02e84
Justin Muller <just@uber.com>
2020-06-02 16:38:43 -0700

    adding fallback option if release has trouble loading. lets user visit github link instead.

--- news/blog/.vuepress/components/release-notes.vue
+++ news/blog/.vuepress/components/release-notes.vue
@@ -10,18 +10,30 @@ export default {
   props: ['owner', 'repo', 'tag'],
   data() {
     return {
-      body: ''
+      body: '',
     };
   },
+  computed: {
+    githubLink() {
+      return `https://github.com/${this.owner}/${this.repo}/releases/tag/${this.tag}`;
+    },
+    githubApi() {
+      return `https://api.github.com/repos/${this.owner}/${this.repo}/releases/tags/${this.tag}`;
+    },
+  },
   async beforeMount() {
-    const response = await fetch(`https://api.github.com/repos/${this.owner}/${this.repo}/releases/tags/${this.tag}`);
-    const releaseData = await response.json();
-    const md = new MarkdownIt();
-    const rawBody = md.render(releaseData.body);
-    this.body = rawBody
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>')
-      .replace(/&quot;/g, '"');
+    try {
+      const response = await fetch(this.githubApi);
+      const releaseData = await response.json();
+      const md = new MarkdownIt();
+      const rawBody = md.render(releaseData.body);
+      this.body = rawBody
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&quot;/g, '"');
+    } catch {
+      this.body = `<p>Please see release notes <a href="${this.githubLink}" target="_blank" rel="noopener noreferrer">here</a></p>`;
+    }
   }
 };
 </script>
