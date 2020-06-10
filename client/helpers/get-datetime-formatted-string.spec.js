4e082182b268872835caa7814634afae3ec3e6a8
Justin Muller <just@uber.com>
2020-06-10 11:38:25 -0700

    fixing lint

--- client/helpers/get-datetime-formatted-string.spec.js
+++ client/helpers/get-datetime-formatted-string.spec.js
@@ -7,12 +7,10 @@ import {
   TIMEZONE_LOCAL,
   TIMEZONE_UTC,
 } from '../constants';
-import {
-  default as getDateTimeFormattedString,
+import getDateTimeFormattedString, {
   getDateFormat,
   getTimeFormat,
   getDateTimeFormat,
-  getMomentFn,
 } from './get-datetime-formatted-string';
 
 describe('getDateTimeFormattedString', () => {
@@ -20,18 +18,21 @@ describe('getDateTimeFormattedString', () => {
     it('should return "YYYY-MM-DD" when dateFormat = DATE_FORMAT_YYYY_MM_DD.', () => {
       const dateFormat = DATE_FORMAT_YYYY_MM_DD;
       const output = getDateFormat(dateFormat);
+
       expect(output).toEqual('YYYY-MM-DD');
     });
 
     it('should return "D MMM, YYYY" when dateFormat = DATE_FORMAT_D_MMM_YYYY.', () => {
       const dateFormat = DATE_FORMAT_D_MMM_YYYY;
       const output = getDateFormat(dateFormat);
+
       expect(output).toEqual('D MMM, YYYY');
     });
 
     it('should return "MMM D, YYYY" when dateFormat = DATE_FORMAT_MMM_D_YYYY.', () => {
       const dateFormat = DATE_FORMAT_MMM_D_YYYY;
       const output = getDateFormat(dateFormat);
+
       expect(output).toEqual('MMM D, YYYY');
     });
   });
@@ -40,12 +41,14 @@ describe('getDateTimeFormattedString', () => {
     it('should return "HH:mm:ss" when timeFormat = TIME_FORMAT_24.', () => {
       const timeFormat = TIME_FORMAT_24;
       const output = getTimeFormat(timeFormat);
+
       expect(output).toEqual('HH:mm:ss');
     });
 
     it('should return "h:mm:ss A" when timeFormat = TIME_FORMAT_12.', () => {
       const timeFormat = TIME_FORMAT_12;
       const output = getTimeFormat(timeFormat);
+
       expect(output).toEqual('h:mm:ss A');
     });
   });
@@ -55,6 +58,7 @@ describe('getDateTimeFormattedString', () => {
       const dateFormat = DATE_FORMAT_YYYY_MM_DD;
       const timeFormat = TIME_FORMAT_24;
       const output = getDateTimeFormat(dateFormat, timeFormat);
+
       expect(output).toEqual('YYYY-MM-DD HH:mm:ss');
     });
 
@@ -62,6 +66,7 @@ describe('getDateTimeFormattedString', () => {
       const dateFormat = DATE_FORMAT_D_MMM_YYYY;
       const timeFormat = TIME_FORMAT_12;
       const output = getDateTimeFormat(dateFormat, timeFormat);
+
       expect(output).toEqual('D MMM, YYYY h:mm:ss A');
     });
   });
@@ -72,7 +77,13 @@ describe('getDateTimeFormattedString', () => {
       const dateFormat = DATE_FORMAT_YYYY_MM_DD;
       const timeFormat = TIME_FORMAT_24;
       const timezone = TIMEZONE_UTC;
-      const output = getDateTimeFormattedString({ date, dateFormat, timeFormat, timezone });
+      const output = getDateTimeFormattedString({
+        date,
+        dateFormat,
+        timeFormat,
+        timezone,
+      });
+
       expect(output).toEqual('2020-01-01 00:00:00');
     });
 
@@ -81,7 +92,13 @@ describe('getDateTimeFormattedString', () => {
       const dateFormat = DATE_FORMAT_D_MMM_YYYY;
       const timeFormat = TIME_FORMAT_12;
       const timezone = TIMEZONE_LOCAL;
-      const output = getDateTimeFormattedString({ date, dateFormat, timeFormat, timezone });
+      const output = getDateTimeFormattedString({
+        date,
+        dateFormat,
+        timeFormat,
+        timezone,
+      });
+
       expect(output).toEqual('31 Dec, 2019 4:00:00 PM');
     });
   });
