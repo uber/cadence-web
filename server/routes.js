a8fd9fffa014859c44349c27dea5b1b863842b77
Justin Muller <just@uber.com>
2020-06-12 17:06:56 -0700

    working on fetching last history item and inserting it into request if workflow is not running.

--- server/routes.js
+++ server/routes.js
@@ -188,7 +188,18 @@ router.post('/api/domains/:domain/workflows/:workflowId/:runId/signal/:signal', 
 
 router.get('/api/domains/:domain/workflows/:workflowId/:runId', async function (ctx) {
   try {
-    ctx.body = await ctx.cadence.describeWorkflow();
+    const describeResponse = await ctx.cadence.describeWorkflow();
+
+    // TODO - check if workflow is still running - i.e. don't populate result if it is still running...
+
+    /*
+    const lastEventResponse = await ctx.cadence.getHistory({
+      HistoryEventFilterType: 'CLOSE_EVENT',
+    });
+    describeResponse.workflowExecutionInfo.result = mapHistoryResponse(lastEventResponse.history)[0];
+    */
+
+    ctx.body = describeResponse;
   } catch (error) {
     if (error.name !== 'NotFoundError') {
       throw error;
