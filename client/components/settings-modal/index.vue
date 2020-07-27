172b769a017a531e798f9835cc1a150c108cc982
Justin Muller <just@uber.com>
2020-07-27 14:46:32 -0700

    Add workflow history screen to settings modal

--- client/components/settings-modal/index.vue
+++ client/components/settings-modal/index.vue
@@ -10,24 +10,50 @@
         title-suffix="Settings"
         @close="onClose"
       />
-      <settings-date-format
-        :date-format="dateFormat"
-        :date-format-options="dateFormatOptions"
-        :time-format="timeFormat"
-        :time-format-options="timeFormatOptions"
-        :timezone="timezone"
-        :timezone-options="timezoneOptions"
-        v-if="settingsDateFormatViewActive"
-        @change="onSettingsChange"
-        @close="onClose"
-      />
+      <flex-grid>
+        <flex-grid-item margin="20px">
+          <settings-list
+            :active-view-name="activeView.name"
+            :view-list="viewList"
+            @change="onSettingsListChange"
+          />
+        </flex-grid-item>
+        <flex-grid-item>
+          <settings-date-format
+            :date-format="dateFormat"
+            :date-format-options="dateFormatOptions"
+            :time-format="timeFormat"
+            :time-format-options="timeFormatOptions"
+            :timezone="timezone"
+            :timezone-options="timezoneOptions"
+            v-if="settingsDateFormatViewActive"
+            @change="onSettingsChange"
+            @close="onClose"
+          />
+          <settings-workflow-history
+            :workflow-history-event-highlight-list="
+              workflowHistoryEventHighlightList
+            "
+            :workflow-history-event-highlight-list-enabled="
+              workflowHistoryEventHighlightListEnabled
+            "
+            v-if="settingsWorkflowHistoryViewActive"
+            @change="onSettingsChange"
+            @close="onClose"
+          />
+        </flex-grid-item>
+      </flex-grid>
     </div>
   </modal>
 </template>
 
 <script>
+import FlexGrid from '../flex-grid';
+import FlexGridItem from '../flex-grid-item';
 import SettingsDateFormat from './components/settings-date-format';
 import SettingsHeader from './components/settings-header';
+import SettingsList from './components/settings-list';
+import SettingsWorkflowHistory from './components/settings-workflow-history';
 import { SETTINGS_VIEW_LIST } from './constants';
 
 export default {
@@ -67,6 +93,9 @@ export default {
     settingsDateFormatViewActive() {
       return this.activeView.name === 'settings-date-format';
     },
+    settingsWorkflowHistoryViewActive() {
+      return this.activeView.name === 'settings-workflow-history';
+    },
   },
   methods: {
     close() {
@@ -85,9 +114,16 @@ export default {
       this.$emit('change', event);
       this.close();
     },
+    onSettingsListChange({ view }) {
+      this.activeView = view;
+    },
   },
   components: {
+    'flex-grid': FlexGrid,
+    'flex-grid-item': FlexGridItem,
     'settings-date-format': SettingsDateFormat,
+    'settings-list': SettingsList,
+    'settings-workflow-history': SettingsWorkflowHistory,
     'settings-header': SettingsHeader,
   },
 };
