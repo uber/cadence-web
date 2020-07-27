6553d4ad8ffe478b276ee386b602076a54576517
Justin Muller <just@uber.com>
2020-07-27 09:01:34 -0700

    fixing up test description

--- client/helpers/parse-string-to-boolean.spec.js
+++ client/helpers/parse-string-to-boolean.spec.js
@@ -1,12 +1,12 @@
 import parseStringToBoolean from './parse-string-to-boolean';
 
 describe('parseStringToBoolean', () => {
-  it('should return true when passed "true".', () => {
+  it('should return true when passed value = "true".', () => {
     const output = parseStringToBoolean('true');
     expect(output).toEqual(true);
   });
 
-  it('should return false when passed "false".', () => {
+  it('should return false when passed value = "false".', () => {
     const output = parseStringToBoolean('false');
     expect(output).toEqual(false);
   });
@@ -16,7 +16,7 @@ describe('parseStringToBoolean', () => {
     expect(output).toEqual(false);
   });
 
-  it('should return true when null is passed and default is set to true.', () => {
+  it('should return true when passed value = null and defaultValue = true.', () => {
     const output = parseStringToBoolean(null, true);
     expect(output).toEqual(true);
   });
