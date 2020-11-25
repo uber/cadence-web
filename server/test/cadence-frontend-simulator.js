b4a35ad24626bdff9894b21c5f868e51cb4e324a
Justin Muller <just@uber.com>
2020-11-24 16:08:37 -0800

    running lint auto-fix

--- server/test/cadence-frontend-simulator.js
+++ server/test/cadence-frontend-simulator.js
@@ -1,42 +1,44 @@
-const
-  supertest = require('supertest'),
+const supertest = require('supertest'),
   TChannel = require('tchannel'),
   TChannelAsThrift = require('tchannel/as/thrift'),
   Long = require('long'),
-  path = require('path')
+  path = require('path');
 
-var tchanServer, currTest, client, app
+let tchanServer, currTest, client, app;
 
-global.should = require('chai').should()
-global.dateToLong = d => Long.fromValue(Number(new Date(d))).mul(1000000)
+global.should = require('chai').should();
+
+global.dateToLong = d => Long.fromValue(Number(new Date(d))).mul(1000000);
 
 before(function(done) {
-  tchanServer = new TChannel({ serviceName: 'cadence-frontend' })
+  tchanServer = new TChannel({ serviceName: 'cadence-frontend' });
 
-  client = new TChannel()
-  var cadenceChan = client.makeSubChannel({
-    serviceName: 'cadence-frontend'
-  })
-  var tchan = TChannelAsThrift({
+  client = new TChannel();
+  const cadenceChan = client.makeSubChannel({
+    serviceName: 'cadence-frontend',
+  });
+  const tchan = TChannelAsThrift({
     channel: cadenceChan,
-    entryPoint: path.join(__dirname, '../idl/cadence.thrift')
-  })
+    entryPoint: path.join(__dirname, '../idl/cadence.thrift'),
+  });
 
   const handler = (ctx, req, head, body, cb) => {
-    var mockName = req.endpoint.replace('WorkflowService::', '')
+    const mockName = req.endpoint.replace('WorkflowService::', '');
+
     if (!currTest[mockName]) {
-      throw new Error(`unexpected request to ${req.endpoint}`)
+      throw new Error(`unexpected request to ${req.endpoint}`);
     }
 
-    var body = currTest[mockName](body, req)
+    var body = currTest[mockName](body, req);
+
     if (body instanceof Error) {
-      cb(body)
+      cb(body);
     } else if (body && body.ok === false) {
-      cb(null, body)
+      cb(null, body);
     } else {
-      cb(null, { ok: true, head, body })
+      cb(null, { ok: true, head, body });
     }
-  }
+  };
 
   [
     'ListOpenWorkflowExecutions',
@@ -48,22 +50,26 @@ before(function(done) {
     'SignalWorkflowExecution',
     'ListDomains',
     'DescribeDomain',
-    'DescribeTaskList'
-  ].forEach(endpoint => tchan.register(tchanServer, 'WorkflowService::' + endpoint, {}, handler))
+    'DescribeTaskList',
+  ].forEach(endpoint =>
+    tchan.register(tchanServer, 'WorkflowService::' + endpoint, {}, handler)
+  );
 
-  process.env.CADENCE_TCHANNEL_PEERS = '127.0.0.1:11343'
-  tchanServer.listen(11343, '127.0.0.1', () => done())
+  process.env.CADENCE_TCHANNEL_PEERS = '127.0.0.1:11343';
+  tchanServer.listen(11343, '127.0.0.1', () => done());
 
-  app = require('../').init({ useWebpack: false, logErrors: false }).listen()
-  global.request = supertest.bind(supertest, app)
-})
+  app = require('../')
+    .init({ useWebpack: false, logErrors: false })
+    .listen();
+  global.request = supertest.bind(supertest, app);
+});
 
 after(function() {
-  app.close()
-  tchanServer.close()
-  client.close()
-})
+  app.close();
+  tchanServer.close();
+  client.close();
+});
 
 beforeEach(function() {
-  currTest = this.currentTest
-})
+  currTest = this.currentTest;
+});
--- server/test/history.test.js
+++ server/test/history.test.js
@@ -1,92 +1,104 @@
 const Long = require('long'),
-
-wfHistoryThrift = [{
-  eventId: new Long(1),
-  timestamp: new Long(800610625, 351737684, false),
-  eventType: 'WorkflowExecutionStarted',
-  workflowExecutionStartedEventAttributes: {
-    attempt: null,
-    workflowType: {
-      name: 'github.com/uber/cadence/demo'
+  wfHistoryThrift = [
+    {
+      eventId: new Long(1),
+      timestamp: new Long(800610625, 351737684, false),
+      eventType: 'WorkflowExecutionStarted',
+      workflowExecutionStartedEventAttributes: {
+        attempt: null,
+        workflowType: {
+          name: 'github.com/uber/cadence/demo',
+        },
+        taskList: {
+          name: 'ci-task-queue',
+          kind: null,
+        },
+        identity: null,
+        input: Buffer.from(
+          JSON.stringify({
+            emails: ['jane@example.com', 'bob@example.com'],
+            includeFooter: true,
+          })
+        ),
+        expirationTimestamp: null,
+        continuedExecutionRunId: null,
+        continuedFailureDetails: null,
+        continuedFailureReason: null,
+        cronSchedule: null,
+        firstDecisionTaskBackoffSeconds: null,
+        firstExecutionRunId: null,
+        header: null,
+        initiator: null,
+        lastCompletionResult: null,
+        memo: null,
+        originalExecutionRunId: null,
+        parentInitiatedEventId: null,
+        parentWorkflowDomain: null,
+        parentWorkflowExecution: null,
+        prevAutoResetPoints: null,
+        retryPolicy: null,
+        searchAttributes: null,
+        taskStartToCloseTimeoutSeconds: 30,
+        executionStartToCloseTimeoutSeconds: 1080,
+      },
     },
-    taskList: {
-      name: 'ci-task-queue',
-      kind: null
+    {
+      eventId: new Long(2),
+      timestamp: new Long(800610625, 351737684, false),
+      eventType: 'DecisionTaskScheduled',
+      decisionTaskScheduledEventAttributes: {
+        startToCloseTimeoutSeconds: 180,
+        attempt: 1,
+        taskList: {
+          name: 'canary-task-queue',
+          kind: null,
+        },
+      },
     },
-    identity: null,
-    input: Buffer.from(JSON.stringify({
-      emails: ['jane@example.com', 'bob@example.com'],
-      includeFooter: true
-    })),
-    expirationTimestamp: null,
-    continuedExecutionRunId: null,
-    continuedFailureDetails: null,
-    continuedFailureReason: null,
-    cronSchedule: null,
-    firstDecisionTaskBackoffSeconds: null,
-    firstExecutionRunId: null,
-    header: null,
-    initiator: null,
-    lastCompletionResult: null,
-    memo: null,
-    originalExecutionRunId: null,
-    parentInitiatedEventId: null,
-    parentWorkflowDomain: null,
-    parentWorkflowExecution: null,
-    prevAutoResetPoints: null,
-    retryPolicy: null,
-    searchAttributes: null,
-    taskStartToCloseTimeoutSeconds: 30,
-    executionStartToCloseTimeoutSeconds: 1080
-  }
-}, {
-  eventId: new Long(2),
-  timestamp: new Long(800610625, 351737684, false),
-  eventType: 'DecisionTaskScheduled',
-  decisionTaskScheduledEventAttributes: {
-    startToCloseTimeoutSeconds: 180,
-    attempt: 1,
-    taskList: {
-      name: 'canary-task-queue',
-      kind: null
-    }
-  }
-}, {
-  eventId: new Long(3),
-  timestamp: new Long(800610625, 351737688, false),
-  eventType: 'DecisionTaskStarted',
-  decisionTaskStartedEventAttributes: {
-    identity: 'box1@ci-task-queue',
-    requestId: 'fafa095d-b4ca-423a-a812-223e62b5ccf8',
-    scheduledEventId: new Long(2)
-  }
-}],
-
-wfHistoryJson = [{
-  eventId: 1,
-  timestamp: '2017-11-14T23:24:10.351Z',
-  eventType: 'WorkflowExecutionStarted',
-  details: Object.assign({}, wfHistoryThrift[0].workflowExecutionStartedEventAttributes, {
-    input: {
-      emails: ['jane@example.com', 'bob@example.com'],
-      includeFooter: true
-    }
-  })
-}, {
-  eventId: 2,
-  timestamp: '2017-11-14T23:24:10.351Z',
-  eventType: 'DecisionTaskScheduled',
-  details: wfHistoryThrift[1].decisionTaskScheduledEventAttributes
-}, {
-  eventId: 3,
-  timestamp: '2017-11-14T23:24:27.531Z',
-  eventType: 'DecisionTaskStarted',
-  details: {
-    identity: 'box1@ci-task-queue',
-    requestId: 'fafa095d-b4ca-423a-a812-223e62b5ccf8',
-    scheduledEventId: 2,
-  }
-}]
+    {
+      eventId: new Long(3),
+      timestamp: new Long(800610625, 351737688, false),
+      eventType: 'DecisionTaskStarted',
+      decisionTaskStartedEventAttributes: {
+        identity: 'box1@ci-task-queue',
+        requestId: 'fafa095d-b4ca-423a-a812-223e62b5ccf8',
+        scheduledEventId: new Long(2),
+      },
+    },
+  ],
+  wfHistoryJson = [
+    {
+      eventId: 1,
+      timestamp: '2017-11-14T23:24:10.351Z',
+      eventType: 'WorkflowExecutionStarted',
+      details: Object.assign(
+        {},
+        wfHistoryThrift[0].workflowExecutionStartedEventAttributes,
+        {
+          input: {
+            emails: ['jane@example.com', 'bob@example.com'],
+            includeFooter: true,
+          },
+        }
+      ),
+    },
+    {
+      eventId: 2,
+      timestamp: '2017-11-14T23:24:10.351Z',
+      eventType: 'DecisionTaskScheduled',
+      details: wfHistoryThrift[1].decisionTaskScheduledEventAttributes,
+    },
+    {
+      eventId: 3,
+      timestamp: '2017-11-14T23:24:27.531Z',
+      eventType: 'DecisionTaskStarted',
+      details: {
+        identity: 'box1@ci-task-queue',
+        requestId: 'fafa095d-b4ca-423a-a812-223e62b5ccf8',
+        scheduledEventId: 2,
+      },
+    },
+  ];
 
 describe('Workflow History', function() {
   it('should forward the request to the cadence frontend with workflowId and runId', function() {
@@ -96,38 +108,40 @@ describe('Workflow History', function() {
         domain: 'canary',
         execution: {
           workflowId: 'ci/demo',
-          runId: 'run1'
+          runId: 'run1',
         },
         maximumPageSize: 100,
         nextPageToken: null,
         skipArchival: null,
-        waitForNewEvent: null
-      })
+        waitForNewEvent: null,
+      });
 
       return {
         history: { events: wfHistoryThrift },
-        nextPageToken: new Buffer('page2')
-      }
-    }
+        nextPageToken: new Buffer('page2'),
+      };
+    };
 
     return request()
       .get('/api/domains/canary/workflows/ci%2Fdemo/run1/history')
       .expect(200)
-      .expect('Content-Type', /json/)
-  })
+      .expect('Content-Type', /json/);
+  });
 
   it('should forward the nextPageToken', function() {
     this.test.GetWorkflowExecutionHistory = ({ getRequest }) => {
-      getRequest.nextPageToken.toString().should.equal('page2')
+      getRequest.nextPageToken.toString().should.equal('page2');
 
       return {
         history: { events: [] },
         nextPageToken: new Buffer('page3'),
-      }
-    }
+      };
+    };
 
     return request()
-      .get('/api/domains/canary/workflows/ci%2Fdemo/run1/history?nextPageToken=cGFnZTI%3D')
+      .get(
+        '/api/domains/canary/workflows/ci%2Fdemo/run1/history?nextPageToken=cGFnZTI%3D'
+      )
       .expect(200)
       .expect('Content-Type', /json/)
       .expect({
@@ -135,30 +149,36 @@ describe('Workflow History', function() {
         history: { events: [] },
         nextPageToken: 'cGFnZTM=',
         rawHistory: null,
-      })
-  })
+      });
+  });
 
   it('should support long polling by forwarding the waitForNewEvent flag', function() {
     this.test.GetWorkflowExecutionHistory = ({ getRequest }) => {
-      getRequest.waitForNewEvent.should.be.true
-      return { history: { events: [{ eventId: 1 }] } }
-    }
+      getRequest.waitForNewEvent.should.be.true;
+
+      return { history: { events: [{ eventId: 1 }] } };
+    };
 
     return request()
-      .get('/api/domains/canary/workflows/ci%2Fdemo/run1/history?waitForNewEvent=true')
+      .get(
+        '/api/domains/canary/workflows/ci%2Fdemo/run1/history?waitForNewEvent=true'
+      )
       .expect(200)
       .expect('Content-Type', /json/)
-      .then(() =>  request()
-        .get('/api/domains/canary/workflows/ci%2Fdemo/run1/history?waitForNewEvent')
-        .expect(200)
-      )
-  })
+      .then(() =>
+        request()
+          .get(
+            '/api/domains/canary/workflows/ci%2Fdemo/run1/history?waitForNewEvent'
+          )
+          .expect(200)
+      );
+  });
 
   it('should transform Long numbers to JavaScript numbers, Long dates to ISO date strings, and line-delimited JSON buffers to JSON', function() {
     this.test.GetWorkflowExecutionHistory = ({ getRequest }) => ({
       history: { events: wfHistoryThrift },
       nextPageToken: new Buffer('page2'),
-    })
+    });
 
     return request()
       .get('/api/domains/canary/workflows/ci%2Fdemo/run1/history')
@@ -168,44 +188,48 @@ describe('Workflow History', function() {
         history: { events: wfHistoryJson },
         nextPageToken: 'cGFnZTI=',
         rawHistory: null,
-      })
-  })
+      });
+  });
 
   describe('Export', function() {
-    const wfHistoryCliJson = `[{"eventId":1,"timestamp":1510701850351393089,"eventType":"WorkflowExecutionStarted","workflowExecutionStartedEventAttributes":{"workflowType":{"name":"github.com/uber/cadence/demo"},"taskList":{"name":"ci-task-queue"},"input":"eyJlbWFpbHMiOlsiamFuZUBleGFtcGxlLmNvbSIsImJvYkBleGFtcGxlLmNvbSJdLCJpbmNsdWRlRm9vdGVyIjp0cnVlfQ==","executionStartToCloseTimeoutSeconds":1080,"taskStartToCloseTimeoutSeconds":30}},{"eventId":2,"timestamp":1510701850351393089,"eventType":"DecisionTaskScheduled","decisionTaskScheduledEventAttributes":{"taskList":{"name":"canary-task-queue"},"startToCloseTimeoutSeconds":180,"attempt":1}},{"eventId":3,"timestamp":1510701867531262273,"eventType":"DecisionTaskStarted","decisionTaskStartedEventAttributes":{"scheduledEventId":2,"identity":"box1@ci-task-queue","requestId":"fafa095d-b4ca-423a-a812-223e62b5ccf8"}}]`
+    const wfHistoryCliJson = `[{"eventId":1,"timestamp":1510701850351393089,"eventType":"WorkflowExecutionStarted","workflowExecutionStartedEventAttributes":{"workflowType":{"name":"github.com/uber/cadence/demo"},"taskList":{"name":"ci-task-queue"},"input":"eyJlbWFpbHMiOlsiamFuZUBleGFtcGxlLmNvbSIsImJvYkBleGFtcGxlLmNvbSJdLCJpbmNsdWRlRm9vdGVyIjp0cnVlfQ==","executionStartToCloseTimeoutSeconds":1080,"taskStartToCloseTimeoutSeconds":30}},{"eventId":2,"timestamp":1510701850351393089,"eventType":"DecisionTaskScheduled","decisionTaskScheduledEventAttributes":{"taskList":{"name":"canary-task-queue"},"startToCloseTimeoutSeconds":180,"attempt":1}},{"eventId":3,"timestamp":1510701867531262273,"eventType":"DecisionTaskStarted","decisionTaskStartedEventAttributes":{"scheduledEventId":2,"identity":"box1@ci-task-queue","requestId":"fafa095d-b4ca-423a-a812-223e62b5ccf8"}}]`;
 
     it('should be able to export history in a format compatible with the CLI', function() {
       this.test.GetWorkflowExecutionHistory = ({ getRequest }) => ({
-        history: { events: wfHistoryThrift }
-      })
+        history: { events: wfHistoryThrift },
+      });
 
       return request()
         .get('/api/domains/canary/workflows/ci%2Fdemo/run1/export')
         .expect(200)
-        .expect(wfHistoryCliJson)
-    })
+        .expect(wfHistoryCliJson);
+    });
 
     it('should page through all responses', async function() {
-      var calls = 0
+      let calls = 0;
+
       this.test.GetWorkflowExecutionHistory = ({ getRequest }) => {
         if (calls > 0) {
-          getRequest.nextPageToken.should.be.ok
+          getRequest.nextPageToken.should.be.ok;
         } else {
-          should.not.exist(getRequest.nextPageToken)
+          should.not.exist(getRequest.nextPageToken);
         }
-        var resp = {
-          history: { events: [wfHistoryThrift[calls]] }
-        }
+
+        const resp = {
+          history: { events: [wfHistoryThrift[calls]] },
+        };
+
         if (++calls < wfHistoryThrift.length) {
-          resp.nextPageToken = new Buffer('page' + calls)
+          resp.nextPageToken = new Buffer('page' + calls);
         }
-        return resp
-      }
+
+        return resp;
+      };
 
       return request()
         .get('/api/domains/canary/workflows/ci%2Fdemo/run1/export')
         .expect(200)
-        .expect(wfHistoryCliJson)
-    })
-  })
-})
+        .expect(wfHistoryCliJson);
+    });
+  });
+});
