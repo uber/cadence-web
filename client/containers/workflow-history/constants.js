030a3c6196bd39c9b9c6c3691693f534b4dbf9e2
Justin Muller <just@uber.com>
2021-02-10 10:54:57 -0800

    correcting merge conflict

--- client/containers/workflow-history/constants.js
+++ client/containers/workflow-history/constants.js
@@ -1,3 +1,6 @@
 export const DEFAULT_SPLIT_SIZE_DAG = [40, 60];
 export const DEFAULT_SPLIT_SIZE_TIMELINE = [20, 80];
 export const DEFAULT_SPLIT_SIZE_NONE = [1, 99];
+
+export const GRAPH_VIEW_DAG = 'dag';
+export const GRAPH_VIEW_TIMELINE = 'timeline';
--- client/routes/workflow/constants.js
+++ client/routes/workflow/constants.js
@@ -23,6 +23,3 @@ export const RETRY_COUNT_MAX = 3;
 export const RETRY_TIMEOUT = 6000;
 export const TERMINATE_DEFAULT_ERROR_MESSAGE =
   'An error has occurred. Please check you have the correct permissions to terminate this workflow and try again.';
-
-export const GRAPH_VIEW_DAG = 'dag';
-export const GRAPH_VIEW_TIMELINE = 'timeline';
