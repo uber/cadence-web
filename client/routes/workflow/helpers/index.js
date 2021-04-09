e668511df19e7efb8fbdc091fc3b250bf4abd6b9
Justin <58960161+just-at-uber@users.noreply.github.com>
2021-04-09 16:02:36 -0700

    Refactor getQueryResult to match import pattern for other helpers (#272)

--- client/routes/workflow/helpers/index.js
+++ client/routes/workflow/helpers/index.js
@@ -25,6 +25,7 @@ export { default as getEventFullDetails } from './get-event-full-details';
 export { default as getEventSummary } from './get-event-summary';
 export { default as getHistoryEvents } from './get-history-events';
 export { default as getHistoryTimelineEvents } from './get-history-timeline-events';
+export { default as getQueryResult } from './get-query-result';
 export { default as getSummary } from './get-summary';
 export { default as getSummaryWorkflowStatus } from './get-summary-workflow-status';
 export { default as getTimeElapsedDisplay } from './get-time-elapsed-display';
