ac450f811fb5f4a94d7486274d52e2590a250083
Justin Muller <just@uber.com>
2021-04-12 14:24:29 -0700

    fix lint

--- client/containers/route/getters.spec.js
+++ client/containers/route/getters.spec.js
@@ -1,7 +1,25 @@
-import {
-  ROUTE_PARAMS,
-  ROUTE_QUERY,
-} from './getter-types';
+// Copyright (c) 2021 Uber Technologies Inc.
+//
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import { ROUTE_PARAMS, ROUTE_QUERY } from './getter-types';
 import getters from './getters';
 
 describe('route getters', () => {
@@ -18,6 +36,7 @@ describe('route getters', () => {
 
       it('should return the value from state.route.params', () => {
         const output = getters[ROUTE_PARAMS](state);
+
         expect(output.paramA).toEqual('valueA');
         expect(output.paramB).toEqual('valueB');
       });
@@ -30,6 +49,7 @@ describe('route getters', () => {
 
       it('should return an empty object.', () => {
         const output = getters[ROUTE_PARAMS](state);
+
         expect(output).toEqual({});
       });
     });
@@ -48,6 +68,7 @@ describe('route getters', () => {
 
       it('should return the value from state.route.query', () => {
         const output = getters[ROUTE_QUERY](state);
+
         expect(output.queryA).toEqual('valueA');
         expect(output.queryB).toEqual('valueB');
       });
@@ -60,6 +81,7 @@ describe('route getters', () => {
 
       it('should return an empty object.', () => {
         const output = getters[ROUTE_QUERY](state);
+
         expect(output).toEqual({});
       });
     });
