e70c217a57b7abc8e894bd97541fcbc2f2ac2f5b
Justin Muller <just@uber.com>
2021-04-12 14:03:24 -0700

    adding unit tests for action creator

--- client/containers/route/action-creator.spec.js
+++ client/containers/route/action-creator.spec.js
@@ -1,7 +1,52 @@
-describe('route action creator', () => {
-  describe('', () => {
-    it('', () => {
+import actionCreator from './action-creator';
+import {
+  ROUTE_PUSH,
+  ROUTE_REPLACE,
+  ROUTE_UPDATE_QUERY,
+} from './action-types';
+import { ROUTE_QUERY } from './getter-types';
 
+const getRouterMock = () => ({
+  push: jest.fn(),
+  replace: jest.fn(),
+});
+
+describe('route action creator', () => {
+  describe('when calling actions[ROUTE_PUSH] with "/test"', () => {
+    it('should call router.push with "/test".', () => {
+      const router = getRouterMock();
+      const actions = actionCreator(router);
+      actions[ROUTE_PUSH](null, '/test');
+      expect(router.push).toHaveBeenCalledWith('/test');
+    });
+  });
+
+  describe('when calling actions[ROUTE_REPLACE] with "/test"', () => {
+    it('should call router.replace with "/test".', () => {
+      const router = getRouterMock();
+      const actions = actionCreator(router);
+      actions[ROUTE_REPLACE](null, '/test');
+      expect(router.replace).toHaveBeenCalledWith('/test');
+    });
+  });
+
+  describe('when calling actions[ROUTE_UPDATE_QUERY] with a getter and test object', () => {
+    it('should call router.replace with the original getter query with the test object override.', () => {
+      const router = getRouterMock();
+      const getters = {
+        [ROUTE_QUERY]: {
+          otherQuery: 'otherValue',
+          test: 'valueA',
+        },
+      };
+      const actions = actionCreator(router);
+      actions[ROUTE_UPDATE_QUERY]({ getters }, { test: 'valueB' });
+      expect(router.replace).toHaveBeenCalledWith({
+        query: {
+          otherQuery: 'otherValue',
+          test: 'valueB',
+        },
+      });
     });
   });
 });
