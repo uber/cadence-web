0da33d508c29466228c2c3ae46e0660af196b121
Justin Muller <just@uber.com>
2021-04-26 15:55:32 -0700

    fix lint

--- client/components/domain-navigation.vue
+++ client/components/domain-navigation.vue
@@ -23,8 +23,8 @@
 import debounce from 'lodash-es/debounce';
 import omit from 'lodash-es/omit';
 import { stringify } from 'friendly-querystring';
-import { getKeyValuePairs, mapDomainDescription } from '~helpers';
 import DetailList from './detail-list';
+import { getKeyValuePairs, mapDomainDescription } from '~helpers';
 
 const validationMessages = {
   valid: d => `${d} exists`,
