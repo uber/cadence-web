466d1ac01c9b76dd3c602953c06d28b92b4c6399
Justin Muller <just@uber.com>
2021-04-27 10:36:44 -0700

    increase timeout to 20000ms

--- client/test/workflow.test.js
+++ client/test/workflow.test.js
@@ -337,7 +337,7 @@ describe('Workflow', () => {
 
     describe('Actions', () => {
       it('should offer the user to terminate a running workflow, prompting the user for a termination reason', async function test() {
-        this.timeout(10000);
+        this.timeout(20000);
 
         const [summaryEl] = await summaryTest(this.test);
         const terminateEl = await summaryEl.waitUntilExists(
