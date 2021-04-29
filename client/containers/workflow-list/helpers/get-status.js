8cb732212697a493b57b8bcc7ab20766d373a743
Justin Muller <just@uber.com>
2021-04-29 11:24:41 -0700

    making logic simplier

--- client/containers/workflow-list/helpers/get-status.js
+++ client/containers/workflow-list/helpers/get-status.js
@@ -22,9 +22,7 @@
 import { STATUS_LIST, STATUS_LIST_OPTION_DEFAULT } from '../constants';
 
 const getStatus = status =>
-  !status
-    ? STATUS_LIST_OPTION_DEFAULT
-    : STATUS_LIST.find(item => item.value === status) ||
-      STATUS_LIST_OPTION_DEFAULT;
+  (status && STATUS_LIST.find(item => item.value === status)) ||
+  STATUS_LIST_OPTION_DEFAULT;
 
 export default getStatus;
