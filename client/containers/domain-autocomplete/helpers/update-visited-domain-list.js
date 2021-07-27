24bbbe7acd4de986f51cb34a49a143b9a19031b3
Justin Muller <just@uber.com>
2021-07-26 17:28:54 -0700

    make sure updateVisitedDomainList has a max limit.

--- client/containers/domain-autocomplete/helpers/update-visited-domain-list.js
+++ client/containers/domain-autocomplete/helpers/update-visited-domain-list.js
@@ -19,35 +19,35 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+import { VISITED_DOMAIN_LIST_LIMIT } from '../constants';
+
 const updateVisitedDomainList = ({ value, visitedDomainList }) => {
   const name = typeof value === 'string' ? value : value.domainInfo.name;
   const uuid = typeof value === 'string' ? null : value.domainInfo.uuid;
 
-  const matchedDomainIndex = visitedDomainList.findIndex(visitedDomain =>
-    typeof visitedDomain === 'string'
-      ? visitedDomain === name
-      : visitedDomain.domainInfo.uuid === uuid
+  const matchedDomainIndex = visitedDomainList.findIndex(domain =>
+    typeof domain === 'string'
+      ? domain === name
+      : domain.domainInfo.uuid === uuid
   );
 
   if (matchedDomainIndex === -1) {
-    return [...visitedDomainList, value];
-  }
+    const visitedDomainListExceededLimit =
+      visitedDomainList.length - VISITED_DOMAIN_LIST_LIMIT + 1;
 
-  if (!uuid) {
-    return null;
-  }
-
-  const matchedDomain = visitedDomainList[matchedDomainIndex];
-
-  if (typeof matchedDomain === 'string') {
     return [
-      ...visitedDomainList.slice(0, matchedDomainIndex),
+      ...(visitedDomainListExceededLimit >= 1
+        ? visitedDomainList.slice(visitedDomainListExceededLimit)
+        : visitedDomainList),
       value,
-      ...visitedDomainList.slice(matchedDomainIndex + 1),
     ];
   }
 
-  return null;
+  return [
+    ...visitedDomainList.slice(0, matchedDomainIndex),
+    ...visitedDomainList.slice(matchedDomainIndex + 1),
+    value,
+  ];
 };
 
 export default updateVisitedDomainList;
