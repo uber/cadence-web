810fc600ad32aa4a9b9039e3db06bec857b5c1f1
Justin Muller <just@uber.com>
2021-10-12 16:10:35 -0700

    route getter changes for cross region

--- client/containers/route/getters.js
+++ client/containers/route/getters.js
@@ -20,11 +20,20 @@
 // THE SOFTWARE.
 
 import { get } from 'lodash-es';
-import { ROUTE_PARAMS, ROUTE_PARAMS_DOMAIN, ROUTE_QUERY } from './getter-types';
+import {
+  ROUTE_PARAMS,
+  ROUTE_PARAMS_CLUSTER_NAME,
+  ROUTE_PARAMS_DOMAIN,
+  ROUTE_PARAMS_WORKFLOW_ID,
+  ROUTE_QUERY,
+} from './getter-types';
 
 const getters = {
   [ROUTE_PARAMS]: state => get(state, 'route.params', {}),
+  [ROUTE_PARAMS_CLUSTER_NAME]: (_, getters) =>
+    getters[ROUTE_PARAMS].clusterName,
   [ROUTE_PARAMS_DOMAIN]: (_, getters) => getters[ROUTE_PARAMS].domain,
+  [ROUTE_PARAMS_WORKFLOW_ID]: (_, getters) => getters[ROUTE_PARAMS].workflowId,
   [ROUTE_QUERY]: state => get(state, 'route.query', {}),
 };
 
