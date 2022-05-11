af085c330fb1bb41194cb6fa4a0fd04241a89e54
Justin Muller <just@uber.com>
2022-05-11 09:57:51 -0700

    updating format domain also

--- server/middleware/grpc-client/formatter/index.js
+++ server/middleware/grpc-client/formatter/index.js
@@ -1,7 +1,9 @@
 const formatDefault = require('./format-default');
+const formatDomain = require('./format-domain');
 const formatListDomains = require('./format-list-domains');
 
 module.exports = {
   formatDefault,
+  formatDomain,
   formatListDomains,
 };
