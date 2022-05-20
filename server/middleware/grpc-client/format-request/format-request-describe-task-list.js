1d53424094a8f81cd8c9aebde1ab82563cc7b6c1
Justin Muller <just@uber.com>
2022-05-20 15:29:05 -0700

    avoid using map

--- server/middleware/grpc-client/format-request/format-request-describe-task-list.js
+++ server/middleware/grpc-client/format-request/format-request-describe-task-list.js
@@ -1,11 +1,8 @@
-const TASK_LIST_TYPE_MAP = {
-  Activity: 'TASK_LIST_TYPE_ACTIVITY',
-  Decision: 'TASK_LIST_TYPE_DECISION',
-};
-
 const formatRequestDescribeTaskList = ({ taskListType, ...payload }) => ({
   ...payload,
-  taskListType: TASK_LIST_TYPE_MAP[taskListType] ?? 'TASK_LIST_TYPE_INVALID',
+  taskListType: typeof taskListType === 'string'
+    ? `TASK_LIST_TYPE_${taskListType.toUpperCase()}`
+    : 'TASK_LIST_TYPE_INVALID',
 });
 
 module.exports = formatRequestDescribeTaskList;
