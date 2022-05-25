0181f0b42f97fdbee71997ba435b8adb93c91dfd
Justin Muller <just@uber.com>
2022-05-25 10:21:06 -0700

    added JSON support for input property

--- server/middleware/grpc-client/format-response/format-history-event-details/format-input.js
+++ server/middleware/grpc-client/format-response/format-history-event-details/format-input.js
@@ -1,9 +1,23 @@
 const atob = require('atob');
-const formatInput = input => input?.data
-  ? atob(input.data)
-    .replace(/"/g, '')
-    .split('\n')
-    .filter(Boolean)
-  : null;
+const formatInput = input => {
+  const data = input?.data;
+  if (!data) {
+    return null;
+  }
+
+  const parsedData = atob(input.data);
+
+  // try parsing as JSON
+  try {
+    const parsedJson = JSON.parse(parsedData);
+    return parsedJson;
+  } catch (e) {
+    // otherwise return as an Array
+    return parsedData
+      .replace(/"/g, '')
+      .split('\n')
+      .filter(Boolean)
+  }
+}
 
 module.exports = formatInput;
