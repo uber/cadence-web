03e50cc47782ad7411a601d18855d187d29d2ecb
Justin Muller <just@uber.com>
2022-06-21 16:06:20 -0700

    reverting constants

--- server/constants.js
+++ server/constants.js
@@ -19,8 +19,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// const PEERS_DEFAULT = '127.0.0.1:7933';
-const PEERS_DEFAULT = '127.0.0.1:5435';
+const PEERS_DEFAULT = '127.0.0.1:7933';
 
 const REQUEST_RETRY_FLAGS_DEFAULT = { onConnectionError: true };
 
@@ -28,11 +27,9 @@ const REQUEST_RETRY_LIMIT_DEFAULT = 3;
 
 const REQUEST_TIMEOUT_DEFAULT = 1000 * 60 * 5;
 
-// const SERVICE_NAME_DEFAULT = 'cadence-frontend';
-const SERVICE_NAME_DEFAULT = 'cadence-frontend-staging2';
+const SERVICE_NAME_DEFAULT = 'cadence-frontend';
 
-// const TRANSPORT_CLIENT_TYPE_DEFAULT = 'tchannel'; // 'tchannel', 'grpc'
-const TRANSPORT_CLIENT_TYPE_DEFAULT = 'grpc'; // 'tchannel', 'grpc'
+const TRANSPORT_CLIENT_TYPE_DEFAULT = 'tchannel'; // 'tchannel', 'grpc'
 
 module.exports = {
   PEERS_DEFAULT,
