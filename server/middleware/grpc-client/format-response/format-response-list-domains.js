d1067d8d288e9ff46f7c8afa4692036ad7405d83
Gaziza Yestemirova <132356583+gazi-yestemirova@users.noreply.github.com>
2023-06-09 11:30:14 +0200

    Update copyright headers (#511)

--- server/middleware/grpc-client/format-response/format-response-list-domains.js
+++ server/middleware/grpc-client/format-response/format-response-list-domains.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Uber Technologies Inc.
+// Copyright (c) 2022-2023 Uber Technologies Inc.
 //
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
