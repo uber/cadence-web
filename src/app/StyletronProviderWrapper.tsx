7b92133419a2ee23ca0ea88cc39c2f17b9e02f0e
Assem Hafez <assem.hafez@uber.com>
2024-02-18 23:24:28 +0100

    fix type checks

--- src/app/StyletronProviderWrapper.tsx
+++ src/app/StyletronProviderWrapper.tsx
@@ -1,10 +1,11 @@
 'use client';
+import React from "react";
 import { Provider as StyletronProvider } from "styletron-react";
 import { ThemeProvider } from "baseui/styles";
 import { DarkTheme } from "baseui/themes";
 import { styletron } from "../styletron";
 
-export default function StyletronProviderWrapper({ children }) {
+export default function StyletronProviderWrapper({ children }: { children: React.ReactNode }) {
     return (
         <StyletronProvider value={styletron}>
             <ThemeProvider theme={DarkTheme}>
--- src/app/registry.tsx
+++ src/app/registry.tsx
@@ -2,13 +2,14 @@
 import React, { useRef } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { styletron, } from '@/styletron';
+import { Server } from 'styletron-engine-monolithic';
 
 export default function StyledJsxRegistry() {
     const isServerInserted = useRef(false);     
     useServerInsertedHTML(() => {
         if (!isServerInserted.current) {
             isServerInserted.current = true;
-            const stylesheets = styletron.getStylesheets();
+            const stylesheets = (styletron as Server).getStylesheets();
             return <>{stylesheets.map((sheet, i) => (
                 <style
                     key={i}
