2f437d3e3a5919b844a853947e15a2f56faaeb34
Assem Hafez <assem.a.hafez@gmail.com>
2024-04-05 16:36:47 +0200

    return mistakenly removed nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, './src'),
+        };
+
+        return config;
+    },
+};
 
 export default nextConfig;
