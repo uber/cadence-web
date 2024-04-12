2e30dad7a82010af189b7d550971b65f0af5077f
Assem Hafez <assem.a.hafez@gmail.com>
2024-04-12 16:16:11 +0000

    make peers and cluster name enviroment varaiable accessible to Browser

--- src/config/grpc/grpc-services-names.ts
+++ src/config/grpc/grpc-services-names.ts
@@ -1,5 +1,5 @@
 
-const envServicesNames = process.env.CADENCE_GRPC_SERVICES_NAMES;
+const envServicesNames = process.env.NEXT_PUBLIC_CADENCE_GRPC_SERVICES_NAMES;
 const GRPC_SERVICES_NAMES = envServicesNames ?  envServicesNames.split(',').filter((s) => Boolean(s.trim())).map((s) => s.trim()) : ['cadence-frontend'];
 
 
