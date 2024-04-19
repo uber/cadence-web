5fd7b6ea3a6c5297b90a71f78ac5400b22800f18
Assem Hafez <assem.a.hafez@gmail.com>
2024-04-19 07:47:51 +0000

    add md icons as overrides to baseweb

--- src/config/theme/theme-light.config.ts
+++ src/config/theme/theme-light.config.ts
@@ -1,5 +1,8 @@
+import baseWebIconsOverrides from "./base-web-icons-ovverrides.config";
+
 const themeLight = {
   grid: { maxWidth: 1580 },
+  icons: baseWebIconsOverrides
 };
 
 export default themeLight;
