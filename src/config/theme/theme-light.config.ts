d375b67b4f9c1fd2a00133f2a99471695601a535
Assem Hafez <assem.a.hafez@gmail.com>
2024-04-19 12:04:52 +0200

    add types to theme-light config

--- src/config/theme/theme-light.config.ts
+++ src/config/theme/theme-light.config.ts
@@ -1,8 +1,9 @@
+import { DeepPartial, MakeExtendable, Theme } from "baseui/styles";
 import baseWebIconsOverrides from "./base-web-icons-ovverrides.config";
 
 const themeLight = {
   grid: { maxWidth: 1580 },
-  icons: baseWebIconsOverrides
-};
+  icons: baseWebIconsOverrides,
+} satisfies DeepPartial<MakeExtendable<Theme>>;
 
 export default themeLight;
