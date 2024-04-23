e1b3b1c0da51284d4bb904695154a3d98dde4922
Assem Hafez <137278762+Assem-Uber@users.noreply.github.com>
2024-04-23 13:45:52 +0200

    add md icons as overrides to baseweb (#552)
    * add md icons as overrides to baseweb
    
    * add types to theme-light config
    
    ---------
    
    Authored-by: Assem Hafez <assem.a.hafez@gmail.com>

--- src/config/theme/theme-light.config.ts
+++ src/config/theme/theme-light.config.ts
@@ -1,5 +1,9 @@
+import { DeepPartial, MakeExtendable, Theme } from "baseui/styles";
+import baseWebIconsOverrides from "./base-web-icons-ovverrides.config";
+
 const themeLight = {
   grid: { maxWidth: 1580 },
-};
+  icons: baseWebIconsOverrides,
+} satisfies DeepPartial<MakeExtendable<Theme>>;
 
 export default themeLight;
