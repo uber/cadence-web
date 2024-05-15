73ee419ea4132437840fe228e2e1bfcab47b8f45
Assem Hafez <assem.a.hafez@gmail.com>
2024-05-15 08:44:38 +0200

    page-section component for handling page spacing

---
+++ src/components/page-section/__test__/page-section.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@/test-utils/rtl';
+import PageSection from '../page-section';
+
+describe('PageSection', () => {
+  it('should use section tag by default', () => {
+    const { getByRole } = render(<PageSection aria-label="test label">Test Content</PageSection>);
+    const sectionElement = getByRole('region', { name: 'test label' });
+    expect(sectionElement).toBeInTheDocument();
+    expect(sectionElement.tagName).toBe('SECTION');
+  });
+  it('renders with custom element', () => {
+    const { getByRole } = render(<PageSection as="nav">Test Content</PageSection>);
+    const sectionElement = getByRole('navigation', { name: '' });
+    expect(sectionElement).toBeInTheDocument();
+    expect(sectionElement.tagName).toBe('NAV');
+  });
+  it('renders provided children', () => {
+    const { getByText } = render(<PageSection as="menu">Test menu</PageSection>);
+    const sectionElement = getByText('Test menu');
+    expect(sectionElement).toBeInTheDocument();
+  });
+});
---
+++ src/components/page-section/page-section.types.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export type Props<T extends React.ComponentType<any> | keyof JSX.IntrinsicElements> = {
+  children: React.ReactNode;
+  as?: T;
+} & Omit<React.ComponentPropsWithoutRef<T>, 'as'>;
