b50d97572621bc629cea23a796c5dd885ac600b1
Assem Hafez <assem.a.hafez@gmail.com>
2024-05-15 10:12:58 +0200

    Page section component (#566)
    * page-section component for handling page spacing
    
    * fix linting

---
+++ src/components/page-section/page-section.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Cell, Grid } from 'baseui/layout-grid';
+import { Props } from './page-section.types';
+
+export default function PageSection<
+  T extends React.ComponentType<any> | keyof JSX.IntrinsicElements = 'section',
+>({ children, as, ...rest }: Props<T>) {
+  const Component = as || 'section';
+  return (
+    <Component {...rest}>
+      <Grid>
+        <Cell span={12}>{children}</Cell>
+      </Grid>
+    </Component>
+  );
+}
