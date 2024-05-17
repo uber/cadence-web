9bcf7524807de91098f67e6e2f7b7c3dc33ab555
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-05-17 13:36:03 +0200

    Fix unit tests

--- src/views/domain-page/domain-page-tabs/__tests__/domain-page-tabs.test.tsx
+++ src/views/domain-page/domain-page-tabs/__tests__/domain-page-tabs.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render, screen } from '@/test-utils/rtl';
+import { render, screen, act, fireEvent } from '@/test-utils/rtl';
 
 import DomainPageTabs from '../domain-page-tabs';
 import domainPageTabsConfig from '../../config/domain-page-tabs.config';
 import { PageTab } from '@/components/page-tabs/page-tabs.types';
+
+const mockPushFn = jest.fn();
+jest.mock('next/navigation', () => ({
+  ...jest.requireActual('next/navigation'),
+  useRouter: () => ({
+    push: mockPushFn,
+    back: () => {},
+    replace: () => {},
+    forward: () => {},
+    prefetch: () => {},
+    refresh: () => {},
+  }),
+  useParams: () => ({
+    domain: 'mock-domain',
+    cluster: 'cluster_1',
+    domainTab: 'workflows',
+  }),
+}));
 
 jest.mock('../../config/domain-page-tabs.config', () => [
   {
@@ -28,6 +46,17 @@ describe('DomainPageTabs', () => {
     domainPageTabsConfig.forEach(({ title }) => {
       expect(screen.getByText(title)).toBeInTheDocument();
     });
+  });
+
+  it('reroutes when new tab is clicked', () => {
+    render(<DomainPageTabs />);
+
+    const page2Tab = screen.getByText('Page 2');
+    act(() => {
+      fireEvent.click(page2Tab);
+    });
+
+    expect(mockPushFn).toHaveBeenCalledWith('page-2');
   });
 
   it('renders tabs artworks correctly', () => {
