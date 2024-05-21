0a91f80c4741b6570916113ad55db29e478acc9f
Mantas Sidlauskas <mantass@netapp.com>
2024-05-21 15:23:28 +0300

    revert constants.js and use them

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,23 @@
+const {
+  PEERS_DEFAULT,
+  REQUEST_RETRY_FLAGS_DEFAULT,
+  REQUEST_RETRY_LIMIT_DEFAULT,
+  REQUEST_TIMEOUT_DEFAULT,
+  SERVICE_NAME_DEFAULT,
+  TRANSPORT_CLIENT_TYPE_DEFAULT,
+} = require('./constants')
+
 module.exports = {
-    peers:  process.env.CADENCE_TCHANNEL_PEERS || '127.0.0.1:7933', 
-    transportClientType: process.env.TRANSPORT_CLIENT_TYPE || 'tchannel', // 'tchannel', 'grpc', // 'tchannel', 'grpc'
+    peers: process.env.CADENCE_TCHANNEL_PEERS || PEERS_DEFAULT,
+    transportClientType: process.env.TRANSPORT_CLIENT_TYPE || TRANSPORT_CLIENT_TYPE_DEFAULT,
     useWebpack: process.env.NODE_ENV !== 'production',
     enableAuth: process.env.ENABLE_AUTH === 'true',
     authType: process.env.AUTH_TYPE,
     authAdminJwtPrivateKey: process.env.AUTH_ADMIN_JWT_PRIVATE_KEY,
     requestConfig: {
-        retryFlags: { onConnectionError: true },
-        retryLimit: process.env.CADENCE_TCHANNEL_RETRY_LIMIT || 3,
-        serviceName: process.env.CADENCE_TCHANNEL_SERVICE || 'cadence-frontend',
-        timeout: 1000 * 60 * 5,
+        retryFlags: REQUEST_RETRY_FLAGS_DEFAULT,
+        retryLimit: process.env.CADENCE_TCHANNEL_RETRY_LIMIT || REQUEST_RETRY_LIMIT_DEFAULT,
+        serviceName: process.env.CADENCE_TCHANNEL_SERVICE || SERVICE_NAME_DEFAULT,
+        timeout: REQUEST_TIMEOUT_DEFAULT,
       }
 }
