e849220fbeafb91648b70689bf4955f78c165bfc
Assem Hafez <assem.a.hafez@gmail.com>
2024-05-23 16:57:38 +0200

    fix linting issues with workflow tab content (#575)
    * fix linting issues with workflow tab content
    
    * fix mock
    
    * fix workflow variable typo

--- src/views/workflow-page/workflow-page-tab-content/__tests__/workflow-page-tab-content.test.tsx
+++ src/views/workflow-page/workflow-page-tab-content/__tests__/workflow-page-tab-content.test.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { render } from '@/test-utils/rtl';
 import WorkflowPageTabContent from '../workflow-page-tab-content';
-import type {
-  WorkflowPageTabContentProps,
-  WorkflowPageTabsContentsMap,
-} from '../workflow-page-tab-content.types';
+import type { WorkflowPageTabContentProps } from '../workflow-page-tab-content.types';
 
-const MockedTabContent = ({ params }: WorkflowPageTabContentProps) => (
-  <div>{JSON.stringify(params)}</div>
-);
-
-const mockedTabContentsMap: WorkflowPageTabsContentsMap = {
-  summary: MockedTabContent,
-};
+jest.mock('../../config/workflow-page-tabs-contents-map.config', () => ({
+  summary: ({ params }: WorkflowPageTabContentProps) => (
+    <div>{JSON.stringify(params)}</div>
+  ),
+}));
 
 const params: WorkflowPageTabContentProps['params'] = {
   cluster: 'example-cluster',
@@ -24,19 +19,14 @@ const params: WorkflowPageTabContentProps['params'] = {
 
 describe('WorkflowPageTabContent', () => {
   it('renders tab content with correct params when workflowTab exists in contentsMap', () => {
-    const { getByText } = render(
-      <WorkflowPageTabContent
-        params={params}
-        tabsContentMap={mockedTabContentsMap}
-      />
-    );
+    const { getByText } = render(<WorkflowPageTabContent params={params} />);
     expect(getByText(JSON.stringify(params))).toBeInTheDocument();
   });
 
   it('does not return any tab cotent if workflowTab is not present in the contentsMap', () => {
     const paramsWithoutTabContent = { ...params, workflowTab: 'unkown-tab' };
-    //@ts-ignore allow passing unknown workflowtab to test recieving wrong value as a param
     const { container } = render(
+      //@ts-ignore allow passing unknown workflowtab to test recieving wrong value as a param
       <WorkflowPageTabContent params={paramsWithoutTabContent} />
     );
     expect(container.firstChild?.textContent).toBe('');
