81e0a751e2f638773d06b1138c2c17a0e736237a
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-05-24 14:43:58 +0200

    init

--- src/views/domain-page/domain-page-workflows/domain-page-workflows.tsx
+++ src/views/domain-page/domain-page-workflows/domain-page-workflows.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { DomainPageTabContentProps } from '../domain-page-content/domain-page-content.types';
 
+import PageFilters from '@/components/page-filters/page-filters';
+
 export default function DomainPageWorkflows(props: DomainPageTabContentProps) {
   return <div>Workflows tab</div>;
 }
