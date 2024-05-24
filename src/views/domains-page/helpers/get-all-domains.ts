adcf34558ee5c7c8fdac3ce34e2d41848558ce69
Assem Hafez <assem.a.hafez@gmail.com>
2024-05-24 06:17:11 +0000

    use functions in getAllDomains

--- src/views/domains-page/helpers/get-all-domains.ts
+++ src/views/domains-page/helpers/get-all-domains.ts
@@ -3,20 +3,18 @@ import * as grpcClient from '@/utils/grpc/grpc-client';
 import { unstable_cache } from 'next/cache';
 import { DomainData } from '../domains-page.types';
 import CLUSTERS_CONFIGS from '@/config/clusters/clusters.config';
+import filterDomainsIrrelevantToCluster from './filter-domains-irrelevant-to-cluster';
+import getUniqueDomains from './get-unique-domains';
 
 export const getAllDomains = async () => {
   const results = await Promise.all(
     CLUSTERS_CONFIGS.map(({ clusterName }) =>
-      grpcClient.clusterMethods[clusterName].listDomains({ pageSize: 1000 })
+      grpcClient.clusterMethods[clusterName].listDomains({ pageSize: 1000 }).then(({ domains }: { domains: DomainData[] }) => {
+        return filterDomainsIrrelevantToCluster(clusterName, domains)
+      })
     )
   );
-  const allUniqueDomains: Record<string, DomainData> = {};
-  results.forEach((res) => {
-    res.domains.forEach((d: DomainData) => {
-      allUniqueDomains[`${d.id}-${d.name}-${d.activeClusterName}`] = d;
-    });
-  });
-  return { domains: Object.values(allUniqueDomains) };
+  return { domains: getUniqueDomains(results.flat(1)) };
 };
 
 export const getCachedAllDomains = cache(
