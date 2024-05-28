391416b731eb1ecdda20670d865d18766fc29442
Assem Hafez <assem.a.hafez@gmail.com>
2024-05-28 14:10:24 +0200

    Domains page links (#580)
    domains list links

---
+++ src/views/domains-page/domains-table-domain-name-cell/__tests__/domains-table-domain-name-cell.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@/test-utils/rtl';
+import DomainsTableDomainNameCell from '../domains-table-domain-name-cell';
+import { globalDomain } from '../../__fixtures__/domains';
+
+describe('DomainTableClusterCell', () => {
+  it('should render link for domain if domain using the active cluster', async () => {
+    render(<DomainsTableDomainNameCell {...globalDomain} />);
+    const clusterLinks = await screen.findAllByRole('link');
+    clusterLinks.forEach((clusterLink, i) => {
+      expect(clusterLink.innerHTML).toBe(globalDomain.name);
+      expect(clusterLink).toHaveAttribute(
+        'href',
+        `/domains/${globalDomain.name}/${globalDomain.activeClusterName}`
+      );
+    });
+  });
+});
---
+++ src/views/domains-page/domains-table-link/domains-table-link.types.ts
@@ -0,0 +1,4 @@
+import Link from 'next/link';
+import React from 'react';
+
+export type Props = React.ComponentProps<typeof Link>;
