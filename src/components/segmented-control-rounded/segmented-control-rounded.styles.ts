e7cd980cc892409d67a211056f2060617a539377
Assem Hafez <assem.a.hafez@gmail.com>
2024-05-28 09:08:54 +0000

    add type to the import of ComponentProps

--- src/components/segmented-control-rounded/segmented-control-rounded.styles.ts
+++ src/components/segmented-control-rounded/segmented-control-rounded.styles.ts
@@ -1,4 +1,4 @@
-import { ComponentProps } from 'react';
+import type { ComponentProps } from 'react';
 import type { StyleObject } from 'styletron-react';
 import type { Theme } from 'baseui';
 import {
