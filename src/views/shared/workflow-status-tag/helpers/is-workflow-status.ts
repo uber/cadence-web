78b4063a2a949db6bfc3d5376876f1af5008ac2e
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-06-03 15:50:18 +0200

    Rename key to value

--- src/views/shared/workflow-status-tag/helpers/is-workflow-status.ts
+++ src/views/shared/workflow-status-tag/helpers/is-workflow-status.ts
@@ -1,6 +1,6 @@
 import { type WorkflowStatus } from '../workflow-status-tag.types';
 import { WORKFLOW_STATUS_NAMES } from '../workflow-status-tag.constants';
 
-export default function isWorkflowStatus(key: any): key is WorkflowStatus {
-  return key in WORKFLOW_STATUS_NAMES;
+export default function isWorkflowStatus(value: any): value is WorkflowStatus {
+  return value in WORKFLOW_STATUS_NAMES;
 }
