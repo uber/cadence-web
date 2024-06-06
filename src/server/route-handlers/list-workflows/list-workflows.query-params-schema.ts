27ee640e9d6af432faabc4a092ccf022fa0514b9
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-06-06 15:47:33 +0200

    Configure input types

--- src/server/route-handlers/list-workflows/list-workflows.query-params-schema.ts
+++ src/server/route-handlers/list-workflows/list-workflows.query-params-schema.ts
@@ -2,25 +2,38 @@ import { z } from 'zod';
 
 import { type SortingOrder } from '@/components/table/table.types';
 import isWorkflowStatus from '@/views/shared/workflow-status-tag/helpers/is-workflow-status';
+import { type WorkflowStatus } from '@/views/shared/workflow-status-tag/workflow-status-tag.types';
+import getTimestampNsFromISO from '@/utils/datetime/get-timestamp-ns-from-iso';
 
 export const listWorkflowsQueryParamSchema = z.object({
   search: z.string().optional(),
   status: z
-    .string()
-    .refine(isWorkflowStatus, { message: 'Invalid workflow status' })
+    .custom<WorkflowStatus>(isWorkflowStatus, {
+      message: 'Invalid workflow status',
+    })
     .optional(),
-  startTimestamp: z.string().datetime().optional(),
-  endTimestamp: z.string().datetime().optional(),
+  startTimestamp: z
+    .string()
+    .datetime()
+    .transform(getTimestampNsFromISO)
+    .optional(),
+  endTimestamp: z
+    .string()
+    .datetime()
+    .transform(getTimestampNsFromISO)
+    .optional(),
   sortColumn: z.string().optional(),
   sortOrder: z
-    .string()
-    .refine((arg): arg is SortingOrder => arg === 'ASC' || arg === 'DESC', {
-      message: 'Invalid sort order',
-    })
+    .custom<SortingOrder>(
+      (arg): arg is SortingOrder => arg === 'ASC' || arg === 'DESC',
+      {
+        message: 'Invalid sort order',
+      }
+    )
     .optional(),
   nextPage: z.string().optional(),
 });
 
-export type ListWorkflowsHandlerQueryParams = z.infer<
+export type ListWorkflowsHandlerQueryParams = z.input<
   typeof listWorkflowsQueryParamSchema
 >;
