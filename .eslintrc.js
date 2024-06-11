bde6f17529ca977f2ba7fbf18fe09367cc4d986f
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-06-11 13:34:56 +0200

    Add import sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,43 @@ module.exports = {
     'next/core-web-vitals',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          'parent',
+          ['sibling', 'index'],
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
