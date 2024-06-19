0a95aa061e8a7c1863ea94eb2354c0be41f48885
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-06-19 15:07:46 +0200

    Remove unused vars exception

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,11 +21,8 @@ module.exports = {
       'warn',
       {
         args: 'all',
-        argsIgnorePattern: '^_',
         caughtErrors: 'all',
-        caughtErrorsIgnorePattern: '^_',
         destructuredArrayIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
         ignoreRestSiblings: true,
       },
     ],
