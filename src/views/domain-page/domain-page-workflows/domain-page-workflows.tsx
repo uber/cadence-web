6c1b369d5f679d64621c9c4659162331f392d05c
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-06-20 14:42:34 +0000

    Move suspense boundary to workflows table

--- src/views/domain-page/domain-page-workflows/domain-page-workflows.tsx
+++ src/views/domain-page/domain-page-workflows/domain-page-workflows.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+
+import SectionLoadingIndicator from '@/components/section-loading-indicator/section-loading-indicator';
 
 import { type DomainPageTabContentProps } from '../domain-page-content/domain-page-content.types';
 import DomainPageWorkflowsFilters from '../domain-page-workflows-filters/domain-page-workflows-filters';
@@ -8,7 +10,12 @@ export default function DomainPageWorkflows(props: DomainPageTabContentProps) {
   return (
     <>
       <DomainPageWorkflowsFilters />
-      <DomainPageWorkflowsTable domain={props.domain} cluster={props.cluster} />
+      <Suspense fallback={<SectionLoadingIndicator />}>
+        <DomainPageWorkflowsTable
+          domain={props.domain}
+          cluster={props.cluster}
+        />
+      </Suspense>
     </>
   );
 }
