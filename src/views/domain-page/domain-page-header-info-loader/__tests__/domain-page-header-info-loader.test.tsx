1cd674e274c496772219c664f9f582bce7297ea4
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-06-26 15:00:08 +0200

    Use react-query describe domain

---
+++ src/views/domain-page/domain-page-header-info-loader/__tests__/domain-page-header-info-loader.test.tsx
@@ -0,0 +1,77 @@
+import { Suspense } from 'react';
+
+import { render, screen, act } from '@/test-utils/rtl';
+
+import * as requestModule from '@/utils/request';
+
+import { mockDomainInfo } from '../../__fixtures__/domain-info';
+import { type DomainInfo } from '../../domain-page.types';
+import DomainPageHeaderInfoLoader from '../domain-page-header-info-loader';
+
+jest.mock('../../domain-page-header-info/domain-page-header-info', () =>
+  jest.fn(
+    ({ domainInfo, cluster }: { domainInfo: DomainInfo; cluster: string }) => (
+      <div>
+        Mock domain info for Domain {domainInfo.name} in {cluster}
+        <div>Domain ID: {domainInfo.id}</div>
+      </div>
+    )
+  )
+);
+
+jest.mock('@/utils/request');
+
+describe(DomainPageHeaderInfoLoader.name, () => {
+  it('renders header info without error', async () => {
+    await setup({});
+
+    expect(
+      await screen.findByText(
+        'Mock domain info for Domain mock-domain-staging in mock-cluster'
+      )
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText('Domain ID: mock-domain-staging-uuid')
+    ).toBeInTheDocument();
+  });
+
+  it('does not render if the initial call fails', async () => {
+    let renderErrorMessage;
+    try {
+      await act(async () => {
+        await setup({ error: true });
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        renderErrorMessage = error.message;
+      }
+    }
+
+    expect(renderErrorMessage).toEqual('Failed to fetch domain info');
+  });
+});
+
+async function setup({ error }: { error?: boolean }) {
+  // TODO: @adhitya.mamallan - This is not type-safe, explore using a library such as nock or msw
+  const requestMock = jest.spyOn(requestModule, 'default') as jest.Mock;
+
+  if (error) {
+    requestMock.mockResolvedValue({
+      ok: false,
+    });
+  } else {
+    requestMock.mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve(mockDomainInfo),
+    });
+  }
+
+  render(
+    <Suspense>
+      <DomainPageHeaderInfoLoader
+        domain="mock-domain-staging"
+        cluster="cluster_1"
+      />
+    </Suspense>
+  );
+}
--- src/views/domain-page/domain-page-header/domain-page-header.tsx
+++ src/views/domain-page/domain-page-header/domain-page-header.tsx
@@ -1,33 +1,37 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 
-import { Cell, Grid } from 'baseui/layout-grid';
 import Image from 'next/image';
 
 import cadenceLogoBlack from '@/assets/cadence-logo-black.svg';
+import PageSection from '@/components/page-section/page-section';
+
+import DomainPageHeaderInfo from '../domain-page-header-info/domain-page-header-info';
+import DomainPageHeaderInfoLoader from '../domain-page-header-info-loader/domain-page-header-info-loader';
 
 import { styled } from './domain-page-header.styles';
 import { type Props } from './domain-page-header.types';
 
 export default function DomainPageHeader(props: Props) {
   return (
-    <section>
-      <Grid>
-        <Cell span={12}>
-          <styled.HeaderContainer>
-            <Image
-              width={32}
-              height={32}
-              alt="Cadence Icon"
-              src={cadenceLogoBlack}
+    <PageSection>
+      <styled.HeaderContainer>
+        <Image
+          width={32}
+          height={32}
+          alt="Cadence Icon"
+          src={cadenceLogoBlack}
+        />
+        <styled.DomainInfoContainer>
+          <styled.DomainNameLabel>{props.domain}</styled.DomainNameLabel>
+          <Suspense fallback={<DomainPageHeaderInfo loading />}>
+            <DomainPageHeaderInfoLoader
+              domain={props.domain}
+              cluster={props.cluster}
             />
-            <styled.DomainInfoContainer>
-              <styled.DomainNameLabel>{props.domain}</styled.DomainNameLabel>
-              {props.domainMetadata}
-            </styled.DomainInfoContainer>
-          </styled.HeaderContainer>
-        </Cell>
-      </Grid>
-    </section>
+          </Suspense>
+        </styled.DomainInfoContainer>
+      </styled.HeaderContainer>
+    </PageSection>
   );
 }
