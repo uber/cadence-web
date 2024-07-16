902cf13892e39b68fc90b837db3620de967780d8
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-07-16 12:38:46 +0000

    Add server action zod schema

--- src/server-actions/update-domain/update-domain.ts
+++ src/server-actions/update-domain/update-domain.ts
@@ -2,31 +2,36 @@
 
 import SECURITY_TOKEN from '@/config/auth/security-token';
 import * as grpcClient from '@/utils/grpc/grpc-client';
-import { GRPCError } from '@/utils/grpc/grpc-error';
 
+import updateDomainValuesSchema from './schemas/update-domain-values-schema';
 import { type Props, type UpdateDomainResponse } from './update-domain.types';
 
 export default async function updateDomain(
   props: Props
 ): Promise<UpdateDomainResponse> {
-  // Zod input validation
+  const { data: values, error } = updateDomainValuesSchema.safeParse(
+    props.values
+  );
+  if (error) {
+    throw new Error('Invalid values: ' + error.message);
+  }
 
   try {
     const res = await grpcClient.getClusterMethods(props.cluster).updateDomain({
       ...props.values,
       name: props.domain,
       securityToken: SECURITY_TOKEN,
-      updateMask: { paths: Object.keys(props.values) },
+      updateMask: { paths: Object.keys(values) },
     });
 
     if (!res.domain) {
-      throw new Error('test');
+      throw new Error('Received empty response');
     }
     return res.domain;
   } catch (e) {
     throw new Error(
       'Error updating domain info: ' +
-        (e instanceof GRPCError ? e.message : 'Unknown error')
+        (e instanceof Error ? e.message : 'Unknown')
     );
   }
 }
