5e38ca186c04d5ea9983bc8d72668e9bed3a8bc3
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-07-19 10:20:10 +0200

    Add success and error toasts for saving domain settings (#613)
    * Add ack toasts
    
    * Update styles
    
    * Style changes
    
    * Update styles again

--- src/views/domain-page/domain-page-settings/domain-page-settings.styles.ts
+++ src/views/domain-page/domain-page-settings/domain-page-settings.styles.ts
@@ -1,7 +1,29 @@
 import { styled as createStyled, type Theme } from 'baseui';
+import { type ToasterOverrides } from 'baseui/toast';
+import { type StyleObject } from 'styletron-react';
 
 export const styled = {
   SettingsContainer: createStyled('div', ({ $theme }: { $theme: Theme }) => ({
     paddingTop: $theme.sizing.scale950,
   })),
+};
+
+export const overrides = {
+  toast: {
+    ToastBody: {
+      style: ({ $theme }: { $theme: Theme }): StyleObject => ({
+        columnGap: $theme.sizing.scale300,
+        [$theme.mediaQuery.medium]: {
+          width: '500px',
+        },
+      }),
+    },
+    ToastCloseIcon: {
+      style: ({ $theme }: { $theme: Theme }): StyleObject => ({
+        alignSelf: 'center',
+        width: $theme.sizing.scale800,
+        height: $theme.sizing.scale800,
+      }),
+    },
+  } satisfies ToasterOverrides,
 };
--- src/views/domain-page/domain-page-settings/domain-page-settings.tsx
+++ src/views/domain-page/domain-page-settings/domain-page-settings.tsx
@@ -6,6 +6,7 @@ import {
   useQueryClient,
   useSuspenseQuery,
 } from '@tanstack/react-query';
+import { toaster, ToasterContainer, PLACEMENT } from 'baseui/toast';
 
 import PageSection from '@/components/page-section/page-section';
 import updateDomain from '@/server-actions/update-domain/update-domain';
@@ -19,7 +20,8 @@ import {
 import { type DomainPageTabContentProps } from '../domain-page-content/domain-page-content.types';
 import { type DomainInfo } from '../domain-page.types';
 
-import { styled } from './domain-page-settings.styles';
+import { SETTINGS_UPDATE_TOAST_DURATION_MS } from './domain-page-settings.constants';
+import { overrides, styled } from './domain-page-settings.styles';
 import { type SettingsValues } from './domain-page-settings.types';
 
 export default function DomainPageSettings(props: DomainPageTabContentProps) {
@@ -61,24 +63,32 @@ export default function DomainPageSettings(props: DomainPageTabContentProps) {
   );
 
   return (
-    <PageSection>
-      <styled.SettingsContainer>
-        <SettingsForm
-          data={domainInfo}
-          zodSchema={domainPageSettingsFormSchema}
-          formConfig={domainPageSettingsFormConfig}
-          onSubmit={async (data) =>
-            await saveSettings.mutateAsync(data).then(() => {
-              queryClient.invalidateQueries({
-                queryKey: ['describeDomain', props],
-              });
-            })
-          }
-          submitButtonText="Save settings"
-          // TODO @adhitya.mamallan - Add logic for an error banner/toast
-          onSubmitError={(e) => console.error(e)}
-        />
-      </styled.SettingsContainer>
-    </PageSection>
+    <ToasterContainer
+      placement={PLACEMENT.bottom}
+      autoHideDuration={SETTINGS_UPDATE_TOAST_DURATION_MS}
+      overrides={overrides.toast}
+    >
+      <PageSection>
+        <styled.SettingsContainer>
+          <SettingsForm
+            data={domainInfo}
+            zodSchema={domainPageSettingsFormSchema}
+            formConfig={domainPageSettingsFormConfig}
+            onSubmit={async (data) =>
+              await saveSettings.mutateAsync(data).then(() => {
+                queryClient.invalidateQueries({
+                  queryKey: ['describeDomain', props],
+                });
+                toaster.positive('Successfully updated domain settings');
+              })
+            }
+            submitButtonText="Save settings"
+            onSubmitError={(e) =>
+              toaster.negative('Error updating domain settings: ' + e.message)
+            }
+          />
+        </styled.SettingsContainer>
+      </PageSection>
+    </ToasterContainer>
   );
 }
