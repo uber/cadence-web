cc9b5421756a1516ac0599272b2d50e35ee00f66
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-08-12 17:33:35 +0200

    Fix issues

--- next-logger.config.js
+++ next-logger.config.js
@@ -2,8 +2,8 @@
   This file is a configuration file for the next-logger library,
   which patches Next's logger to output NDJSON to stdout using Pino.
 
-  next-logger uses its own Pino logger by default, but for a unified logging experience,
-  we pass a sibling logger that is configured similar to the default logger.
+  next-logger uses its own Pino logger by default, but for a unified logging
+  experience, we pass a sibling logger that is configured similar to utils/logger.
 */
 
 const {
--- src/route-handlers/log-to-server/schemas/log-to-server-payload-schema.ts
+++ src/route-handlers/log-to-server/schemas/log-to-server-payload-schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 import { LOG_LEVELS, type LogLevel } from '@/utils/logger';
 
 const logToServerPayloadSchema = z.object({
-  level: z.custom<LogLevel>((v) => LOG_LEVELS.includes(v)),
+  level: z.custom<LogLevel>((v) => LOG_LEVELS.includes(v), 'Invalid log level'),
   message: z.string(),
   payload: z.any(),
 });
