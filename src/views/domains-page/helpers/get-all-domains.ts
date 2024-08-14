6f2f1703c3ed985c72afcfb35bbc8ca40554f253
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-08-14 13:36:58 +0200

    Bump page size and add warning logs

--- src/views/domains-page/helpers/get-all-domains.ts
+++ src/views/domains-page/helpers/get-all-domains.ts
@@ -4,17 +4,29 @@ import { unstable_cache } from 'next/cache';
 
 import CLUSTERS_CONFIGS from '@/config/clusters/clusters.config';
 import * as grpcClient from '@/utils/grpc/grpc-client';
+import logger from '@/utils/logger';
 
 import filterDomainsIrrelevantToCluster from './filter-domains-irrelevant-to-cluster';
 import getUniqueDomains from './get-unique-domains';
+
+const MAX_DOMAINS_TO_FETCH = 2000;
 
 export const getAllDomains = async () => {
   const results = await Promise.all(
     CLUSTERS_CONFIGS.map(({ clusterName }) =>
       grpcClient
         .getClusterMethods(clusterName)
-        .listDomains({ pageSize: 1000 })
+        .listDomains({ pageSize: MAX_DOMAINS_TO_FETCH })
         .then(({ domains }) => {
+          if (domains.length >= MAX_DOMAINS_TO_FETCH - 100) {
+            logger.warn(
+              {
+                domainsCount: domains.length,
+                maxDomainsCount: MAX_DOMAINS_TO_FETCH,
+              },
+              'Number of domains in cluster approaching/exceeds max number of domains that can be fetched'
+            );
+          }
           return filterDomainsIrrelevantToCluster(clusterName, domains);
         })
     )
