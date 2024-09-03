b19996ec99cfe4a806c813be266514a52f3869a4
Assem Hafez <assem.a.hafez@gmail.com>
2024-09-03 13:05:59 +0000

    fix linting issues

--- src/views/workflow-history/workflow-history.tsx
+++ src/views/workflow-history/workflow-history.tsx
@@ -4,15 +4,10 @@ import React from 'react';
 import { HeadingXSmall } from 'baseui/typography';
 
 import useStyletronClasses from '@/hooks/use-styletron-classes';
-import type { WorkflowPageTabContentProps } from '@/views/workflow-page/workflow-page-tab-content/workflow-page-tab-content.types';
 
-import WorkflowHistoryEventStatusBadge from './workflow-history-event-status-badge/workflow-history-event-status-badge';
-import { WORKFLOW_EVENT_STATUS } from './workflow-history-event-status-badge/workflow-history-event-status-badge.constants';
 import { cssStyles } from './workflow-history.styles';
 
-export default function WorkflowHistory({
-  params,
-}: WorkflowPageTabContentProps) {
+export default function WorkflowHistory() {
   const { cls } = useStyletronClasses(cssStyles);
 
   return (
