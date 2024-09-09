b112b997ce792fb38f8f4743ff2e54cec75673fc
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-09-09 15:21:46 +0200

    Use handler filter type directly

--- src/views/task-list-page/config/task-list-filters.config.ts
+++ src/views/task-list-page/config/task-list-filters.config.ts
@@ -1,14 +1,14 @@
-import { type TaskListType } from '@/__generated__/proto-ts/uber/cadence/api/v1/TaskListType';
 import { type PageFilterConfig } from '@/components/page-filters/page-filters.types';
 
 import TaskListFiltersHandlers from '../task-list-filters-handlers/task-list-filters-handlers';
+import { type TaskListFiltersHandlersValue } from '../task-list-filters-handlers/task-list-filters-handlers.types';
 
 import type taskListPageQueryParamsConfig from './task-list-page-query-params.config';
 
 const taskListFiltersConfig: [
   PageFilterConfig<
     typeof taskListPageQueryParamsConfig,
-    { handlerType: TaskListType | undefined }
+    TaskListFiltersHandlersValue
   >,
 ] = [
   {
