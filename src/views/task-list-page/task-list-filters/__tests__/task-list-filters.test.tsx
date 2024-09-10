52e8da9c9901be117dc19c924d2fe86dc6f26175
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-09-10 11:42:08 +0000

    Remove assertions from test

--- src/views/task-list-page/task-list-filters/__tests__/task-list-filters.test.tsx
+++ src/views/task-list-page/task-list-filters/__tests__/task-list-filters.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import { userEvent } from '@testing-library/user-event';
-
-import { render, screen } from '@/test-utils/rtl';
+import { render } from '@/test-utils/rtl';
 
 import { mockQueryParamsValues } from '@/components/page-filters/__fixtures__/page-filters.fixtures';
 
@@ -14,18 +12,7 @@ jest.mock('@/hooks/use-page-query-params/use-page-query-params', () =>
 );
 
 describe(TaskListFilters.name, () => {
-  it('renders without errors', async () => {
-    const user = userEvent.setup();
+  it('renders without errors', () => {
     render(<TaskListFilters />);
-
-    // Assert search bar
-    expect(
-      await screen.findByPlaceholderText('Find worker by identity')
-    ).toBeInTheDocument();
-
-    await user.click(screen.getByText('Filters'));
-    // Assert filters
-    expect(await screen.findByText('Handlers')).toBeInTheDocument();
-    expect(await screen.findByText('Show all handlers')).toBeInTheDocument();
   });
 });
