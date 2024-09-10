a5b52374a62d771bbca2433ed539f61609f88fea
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-09-10 10:50:07 +0000

    Update unit tests

--- src/views/task-list-page/task-list-filters/__tests__/task-list-filters.test.tsx
+++ src/views/task-list-page/task-list-filters/__tests__/task-list-filters.test.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
+import { userEvent } from '@testing-library/user-event';
+
 import { render, screen } from '@/test-utils/rtl';
+
+import { mockQueryParamsValues } from '@/components/page-filters/__fixtures__/page-filters.fixtures';
 
 import TaskListFilters from '../task-list-filters';
 
-jest.mock('@/components/page-filters/page-filters', () =>
-  jest.fn(() => <div>Mock page filters</div>)
+const mockSetQueryParams = jest.fn();
+jest.mock('@/hooks/use-page-query-params/use-page-query-params', () =>
+  jest.fn(() => [mockQueryParamsValues, mockSetQueryParams])
 );
 
 describe(TaskListFilters.name, () => {
-  it('renders without errors', () => {
+  it('renders without errors', async () => {
+    const user = userEvent.setup();
     render(<TaskListFilters />);
 
-    expect(screen.getByText('Mock page filters')).toBeInTheDocument();
+    // Assert search bar
+    expect(
+      await screen.findByPlaceholderText('Find worker by identity')
+    ).toBeInTheDocument();
+
+    await user.click(screen.getByText('Filters'));
+    // Assert filters
+    expect(await screen.findByText('Handlers')).toBeInTheDocument();
+    expect(await screen.findByText('Show all handlers')).toBeInTheDocument();
   });
 });
