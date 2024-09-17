bc3be49791b2a5d2e66318cb6c67619e039b8634
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-09-17 16:59:29 +0200

    Fix unsaved merge conflict

--- src/route-handlers/fetch-workflow-query-types/helpers/__tests__/parse-error-message-for-query-types.test.ts
+++ src/route-handlers/fetch-workflow-query-types/helpers/__tests__/parse-error-message-for-query-types.test.ts
@@ -1,6 +1,5 @@
 import parseErrorMessageForQueryTypes from '../parse-error-message-for-query-types';
 
-<<<<<<< HEAD
 describe(parseErrorMessageForQueryTypes.name, () => {
   it('should parse query types from error message', () => {
     expect(
@@ -19,28 +18,10 @@ describe(parseErrorMessageForQueryTypes.name, () => {
   });
 
   it('should return undefined if the error does not have knownTypes', () => {
-=======
-const MOCK_ERROR_MESSAGE =
-  'java.lang.IllegalArgumentException: Unknown query type: __query_types, knownTypes=[query1, query2]';
-
-describe(parseErrorMessageForQueryTypes.name, () => {
-  it('should parse query types from error message', () => {
-    expect(parseErrorMessageForQueryTypes(MOCK_ERROR_MESSAGE)).toEqual([
-      'query1',
-      'query2',
-    ]);
-  });
-
-  it('should return an empty array if the error does not have known types', () => {
->>>>>>> a3f8a50 (Add tests)
     expect(
       parseErrorMessageForQueryTypes(
         'java.lang.IllegalArgumentException: Something else went wrong'
       )
-<<<<<<< HEAD
     ).toEqual(undefined);
-=======
-    ).toEqual([]);
->>>>>>> a3f8a50 (Add tests)
   });
 });
