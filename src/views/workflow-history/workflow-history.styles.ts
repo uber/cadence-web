3424ab665b38fc34169eaac75f3043f7a170c0c7
Assem Hafez <assem.a.hafez@gmail.com>
2024-09-19 15:19:04 +0000

    history timeline

--- src/views/workflow-history/workflow-history.styles.ts
+++ src/views/workflow-history/workflow-history.styles.ts
@@ -1,7 +1,21 @@
+import { styled as createStyled, type Theme } from 'baseui';
+
 import type {
   StyletronCSSObject,
   StyletronCSSObjectOf,
 } from '@/hooks/use-styletron-classes';
+
+export const styled = {
+  VerticalDivider: createStyled<'div', { $hidden?: boolean }>(
+    'div',
+    ({ $theme, $hidden }: { $theme: Theme; $hidden?: boolean }) => ({
+      ...$theme.borders.border200,
+      borderColor: $theme.colors.borderOpaque,
+      height: $hidden ? 0 : '100%',
+      marginLeft: $theme.sizing.scale500,
+    })
+  ),
+};
 
 const cssStylesObj = {
   pageContainer: {
@@ -11,16 +25,21 @@ const cssStylesObj = {
   eventsContainer: (theme) => ({
     display: 'flex',
     marginTop: theme.sizing.scale500,
+    gap: '10px',
+    overflow: 'hidden',
   }),
   compactSection: {
     display: 'flex',
     flexDirection: 'column',
     width: '370px',
+    gap: '10px',
   },
   timelineSection: {
     display: 'flex',
     flexDirection: 'column',
     flex: 1,
+    paddingLeft: '32px',
+    overflow: 'hidden',
   },
 } satisfies StyletronCSSObject;
 
--- src/views/workflow-history/workflow-history.tsx
+++ src/views/workflow-history/workflow-history.tsx
@@ -1,26 +1,122 @@
 'use client';
-import React from 'react';
+import React, { useMemo } from 'react';
 
+import {
+  useSuspenseInfiniteQuery,
+  type InfiniteData,
+} from '@tanstack/react-query';
 import { HeadingXSmall } from 'baseui/typography';
+import queryString from 'query-string';
 
+import PageSection from '@/components/page-section/page-section';
 import useStyletronClasses from '@/hooks/use-styletron-classes';
+import { type GetWorkflowHistoryResponse } from '@/route-handlers/get-workflow-history/get-workflow-history.types';
+import request from '@/utils/request';
+import { type RequestError } from '@/utils/request/request-error';
+import sortBy from '@/utils/sort-by';
+import type { WorkflowPageTabContentProps } from '@/views/workflow-page/workflow-page-tab-content/workflow-page-tab-content.types';
 
+import { groupHistoryEvents } from './helpers/group-history-events';
+import WorkflowHistoryCompactEventCard from './workflow-history-compact-event-card/workflow-history-compact-event-card';
+import WorkflowHistoryTimelineGroup from './workflow-history-timeline-group/workflow-history-timeline-group';
+import WorkflowHistoryTimelineLoadMore from './workflow-history-timeline-load-more/workflow-history-timeline-load-more';
 import { cssStyles } from './workflow-history.styles';
 
-export default function WorkflowHistory() {
+export default function WorkflowHistory({
+  params,
+}: WorkflowPageTabContentProps) {
   const { cls } = useStyletronClasses(cssStyles);
 
+  const { workflowTab, ...historyQueryParams } = params;
+  const wfhistoryRequestArgs = {
+    ...historyQueryParams,
+    pageSize: 1,
+    waitForNewEvent: 'true',
+  };
+  const {
+    data: result,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    error,
+  } = useSuspenseInfiniteQuery<
+    GetWorkflowHistoryResponse,
+    RequestError,
+    InfiniteData<GetWorkflowHistoryResponse>,
+    [string, typeof wfhistoryRequestArgs],
+    string | undefined
+  >({
+    queryKey: ['workflow_history_paginated', wfhistoryRequestArgs] as const,
+    queryFn: ({ queryKey: [_, qp], pageParam }) =>
+      request(
+        `/api/domains/${qp.domain}/${qp.cluster}/workflows/${qp.workflowId}/${qp.runId}/history?${queryString.stringify({ pageSize: qp.pageSize, nextPage: pageParam, waitForNewEvent: qp.waitForNewEvent })}`
+      ).then((res) => res.json()),
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage?.nextPageToken) return undefined;
+      return lastPage?.nextPageToken;
+    },
+  });
+
+  const workflowHistory = useMemo(() => {
+    return (result.pages || []).flat(1);
+  }, [result]);
+
+  const groupedHistoryEvents = useMemo(() => {
+    const events = workflowHistory
+      .map(({ history }) => history?.events || [])
+      .flat(1);
+    return groupHistoryEvents(events);
+  }, [workflowHistory]);
+  const groupedHistoryEventsEntries = useMemo(() => {
+    return sortBy(
+      Object.entries(groupedHistoryEvents),
+      ([_, { timeMs }]) => timeMs,
+      'ASC'
+    );
+  }, [groupedHistoryEvents]);
+
   return (
-    <div className={cls.pageContainer}>
-      <HeadingXSmall>Workflow history</HeadingXSmall>
-      <div className={cls.eventsContainer}>
-        <section className={cls.compactSection}>
-          <p>compact</p>
-        </section>
-        <section className={cls.timelineSection}>
-          <p>timeline</p>
-        </section>
+    <PageSection>
+      <div className={cls.pageContainer}>
+        <HeadingXSmall>Workflow history</HeadingXSmall>
+        <div className={cls.eventsContainer}>
+          <section className={cls.compactSection}>
+            {groupedHistoryEventsEntries.map(
+              ([groupId, { label, status, timeLabel }]) => (
+                <WorkflowHistoryCompactEventCard
+                  key={groupId}
+                  status={status}
+                  label={label}
+                  secondaryLabel={timeLabel}
+                  showLabelPlaceholder={!label}
+                />
+              )
+            )}
+            
+          </section>
+          <section className={cls.timelineSection}>
+            {groupedHistoryEventsEntries.map(([groupId, group], index) => (
+              <WorkflowHistoryTimelineGroup
+                key={groupId}
+                status={group.status}
+                label={group.label}
+                timeLabel={group.timeLabel}
+                events={group.events}
+                eventsMetadata={group.eventsMetadata}
+                hasMissingEvents={group.hasMissingEvents}
+                isLastEvent={index === groupedHistoryEventsEntries.length - 1}
+              />
+            ))}
+            <WorkflowHistoryTimelineLoadMore
+              error={error}
+              fetchNextPage={fetchNextPage}
+              hasNextPage={hasNextPage}
+              isFetchingNextPage={isFetchingNextPage}
+            />
+          </section>
+        </div>
       </div>
-    </div>
+    </PageSection>
   );
 }
