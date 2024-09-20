508674784222167cf47e7af6aeb15338d53597a7
Assem Hafez <assem.a.hafez@gmail.com>
2024-09-20 12:31:15 +0000

    use Internal error boundary

--- src/views/workflow-page/workflow-page-header/workflow-page-header.tsx
+++ src/views/workflow-page/workflow-page-header/workflow-page-header.tsx
@@ -5,7 +5,6 @@ import { Breadcrumbs } from 'baseui/breadcrumbs';
 import { StyledLink } from 'baseui/link';
 import Image from 'next/image';
 import Link from 'next/link';
-import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
 
 import cadenceLogoBlack from '@/assets/cadence-logo-black.svg';
 import PageSection from '@/components/page-section/page-section';
@@ -15,6 +14,7 @@ import WorkflowPageStatusTag from '../workflow-page-status-tag/workflow-page-sta
 
 import { cssStyles, overrides } from './workflow-page-header.styles';
 import type { Props } from './workflow-page-header.types';
+import ErrorBoundary from '@/components/error-boundary/error-boundary';
 
 export default function WorkflowPageHeader({
   domain,
@@ -49,9 +49,9 @@ export default function WorkflowPageHeader({
         </StyledLink>
         <div className={cls.breadcrumbItemContainer}>
           {runId}
-          <ReactErrorBoundary fallbackRender={() => null}>
+          <ErrorBoundary fallbackRender={() => null} omitLogging={true}>
             <WorkflowPageStatusTag />
-          </ReactErrorBoundary>
+          </ErrorBoundary>
         </div>
       </Breadcrumbs>
     </PageSection>
