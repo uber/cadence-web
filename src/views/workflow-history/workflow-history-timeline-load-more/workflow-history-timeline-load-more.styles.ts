02e913f296dd48ce50ac86a19072969a092e0205
Assem Hafez <assem.a.hafez@gmail.com>
2024-09-20 12:36:03 +0200

    Timeline load more component (#667)
    * group history events
    
    * timeline load more
    
    * Initialize Queries UI (#665)
    
    * Init commit
    
    * more commit
    
    * Add more changes
    
    * Add tests
    
    * clean up code
    
    * Init queries UI
    
    * Add unit tests
    
    * Fix unsaved merge conflict
    
    * Fix tests and merge conflicts again
    
    * update snapshot
    
    * Fix snapshot
    
    * Reorder some props in tile
    
    * resolve comments
    
    * Fix tests
    
    * Fix tests and make design responsive
    
    * Remove snapshots
    
    * Update tests for tile input
    
    * fix retry link spacing
    
    * fix lint
    
    ---------
    
    Co-authored-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

---
+++ src/views/workflow-history/workflow-history-timeline-load-more/workflow-history-timeline-load-more.styles.ts
@@ -0,0 +1,25 @@
+import { styled as createStyled, withStyle } from 'baseui';
+import { StyledLink } from 'baseui/link';
+
+export const styled = {
+  EndMessageContainer: createStyled<'div', { $isError?: boolean }>(
+    'div',
+    ({ $theme, $isError }) => ({
+      margin: '0 auto',
+      padding: $theme.sizing.scale600,
+      ...$theme.typography.LabelSmall,
+      color: $isError
+        ? $theme.colors.contentNegative
+        : $theme.colors.contentTertiary,
+    })
+  ),
+  SpinnerContainer: createStyled('div', ({ $theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    padding: $theme.sizing.scale600,
+  })),
+  RetryLink: withStyle(StyledLink, ({ $theme }) => ({
+    color: $theme.colors.contentNegative,
+    cursor: 'pointer',
+  })),
+};
