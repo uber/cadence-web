9b94f13e96a2384ca2ad6408e44888d0380767e2
Assem Hafez <assem.a.hafez@gmail.com>
2024-09-23 11:58:24 +0200

    Workflow timeline virtual lists

--- src/views/workflow-history/workflow-history.styles.ts
+++ src/views/workflow-history/workflow-history.styles.ts
@@ -1,21 +1,7 @@
-import { styled as createStyled, type Theme } from 'baseui';
-
 import type {
   StyletronCSSObject,
   StyletronCSSObjectOf,
 } from '@/hooks/use-styletron-classes';
-
-export const styled = {
-  VerticalDivider: createStyled<'div', { $hidden?: boolean }>(
-    'div',
-    ({ $theme, $hidden }: { $theme: Theme; $hidden?: boolean }) => ({
-      ...$theme.borders.border200,
-      borderColor: $theme.colors.borderOpaque,
-      height: $hidden ? 0 : '100%',
-      marginLeft: $theme.sizing.scale500,
-    })
-  ),
-};
 
 const cssStylesObj = {
   pageContainer: {
@@ -25,22 +11,29 @@ const cssStylesObj = {
   eventsContainer: (theme) => ({
     display: 'flex',
     marginTop: theme.sizing.scale500,
-    gap: theme.sizing.scale400,
-    overflow: 'hidden',
+    // gap: theme.sizing.scale400,
   }),
   compactSection: (theme) => ({
     display: 'flex',
     flexDirection: 'column',
-    width: '370px',
-    gap: theme.sizing.scale400,
+    width: '362px',
+    maxHeight: `calc(100vh - ${theme.sizing.scale900})`, // fill page height excluding page bottom margin
+    overflow: 'auto',
+    position: 'sticky',
+    top: 0,
+    paddingRight: theme.sizing.scale800,
   }),
-  timelineSection: (theme) => ({
+  compactCardContainer: (theme) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    padding: `${theme.sizing.scale200} 0`,
+    marginRight: theme.sizing.scale700,
+  }),
+  timelineSection: {
     display: 'flex',
     flexDirection: 'column',
     flex: 1,
-    paddingLeft: theme.sizing.scale900,
-    overflow: 'hidden',
-  }),
+  },
 } satisfies StyletronCSSObject;
 
 export const cssStyles: StyletronCSSObjectOf<typeof cssStylesObj> =
