4ce4839cfd49a32fa9ba8ac1bd4c378648bf8913
Assem Hafez <assem.a.hafez@gmail.com>
2024-09-30 11:28:23 +0200

    Create formatters for timers, external workflows & childworkflows  (#683)
    * create formatter functions for workflow execution events
    
    * type cancel request event api
    
    * format activities and decision tasks
    
    * add formatters for remaining files

---
+++ src/utils/data-formatters/format-workflow-history-event/format-child-workflow-execution-failed-event.ts
@@ -0,0 +1,46 @@
+// Copyright (c) 2022-2024 Uber Technologies Inc.
+//
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import formatFailureDetails from '../format-failure-details';
+
+import formatWorkflowCommonEventFields from './format-workflow-common-event-fields';
+import { type ChildWorkflowExecutionFailedEvent } from './format-workflow-history-event.type';
+
+const formatChildWorkflowExecutionFailedEvent = ({
+  childWorkflowExecutionFailedEventAttributes: {
+    failure,
+    initiatedEventId,
+    startedEventId,
+    ...eventAttributes
+  },
+  ...eventFields
+}: ChildWorkflowExecutionFailedEvent) => {
+  return {
+    ...formatWorkflowCommonEventFields(eventFields),
+    ...eventAttributes,
+    details: formatFailureDetails(failure),
+    initiatedEventId: parseInt(initiatedEventId),
+    reason: failure?.reason || '',
+    startedEventId: parseInt(startedEventId),
+  };
+};
+
+export default formatChildWorkflowExecutionFailedEvent;
