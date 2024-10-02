1bfd1d7d3998ff78e0fc218af8dac54594c51220
Assem Hafez <assem.a.hafez@gmail.com>
2024-10-02 19:57:07 +0000

    fix type check

--- src/views/workflow-history/workflow-history-event-details-task-list-link/workflow-history-event-details-task-list-link.types.ts
+++ src/views/workflow-history/workflow-history-event-details-task-list-link/workflow-history-event-details-task-list-link.types.ts
@@ -1,4 +1,4 @@
-import type { Props as WorfklowHistoryProps } from '../../workflow-history.types';
+import type { Props as WorfklowHistoryProps } from '../workflow-history.types';
 
 export type Props = {
   entryKey: string;
