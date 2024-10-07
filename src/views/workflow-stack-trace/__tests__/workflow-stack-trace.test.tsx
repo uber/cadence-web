4cd87e30363ab5a80592642a7ac76d4f67ff6f6f
Assem Hafez <assem.a.hafez@gmail.com>
2024-10-07 08:59:10 +0000

    Add more unit tests

--- src/views/workflow-stack-trace/__tests__/workflow-stack-trace.test.tsx
+++ src/views/workflow-stack-trace/__tests__/workflow-stack-trace.test.tsx
@@ -2,7 +2,12 @@ import { Suspense } from 'react';
 
 import { HttpResponse } from 'msw';
 
-import { act, render, screen, userEvent } from '@/test-utils/rtl';
+import {
+  render,
+  screen,
+  userEvent,
+  waitForElementToBeRemoved,
+} from '@/test-utils/rtl';
 
 import { type QueryWorkflowResponse } from '@/route-handlers/query-workflow/query-workflow.types';
 
@@ -15,6 +20,9 @@ jest.mock('@/components/error-panel/error-panel', () =>
 jest.mock('baseui/skeleton', () => ({
   Skeleton: jest.fn(() => <div>Loading Skeleton</div>),
 }));
+jest.mock('baseui/spinner', () => ({
+  Spinner: jest.fn(() => <div data-testid="spinner"></div>),
+}));
 
 describe('WorkflowStackTrace', () => {
   it('should render correctly with no fetching or error', async () => {
@@ -26,14 +34,12 @@ describe('WorkflowStackTrace', () => {
   });
 
   it('should show loading state on refetch', async () => {
-    const { getRequestResolver } = await setup({ wait: true });
-    const initResolver = getRequestResolver();
-    initResolver();
+    const { getRequestResolver } = await setup({
+      resolveRefreshManually: true,
+    });
 
     const button = await screen.findByRole('button', { name: /Refresh/i });
-    await act(async () => {
-      await userEvent.click(button);
-    });
+    await userEvent.click(button);
     expect(screen.getByText('Loading Skeleton')).toBeInTheDocument();
     expect(screen.queryByText('Error Panel')).not.toBeInTheDocument();
     expect(screen.queryByText('test stack trace')).not.toBeInTheDocument();
@@ -44,21 +50,150 @@ describe('WorkflowStackTrace', () => {
     expect(screen.queryByText('Loading Skeleton')).not.toBeInTheDocument();
     expect(screen.queryByText('Error Panel')).not.toBeInTheDocument();
   });
+
+  it('should disable the refresh button when refetching', async () => {
+    await setup({
+      resolveRefreshManually: true,
+    });
+
+    const refreshButton = await screen.findByRole('button', {
+      name: /Refresh/i,
+    });
+    await userEvent.click(refreshButton);
+
+    expect(refreshButton).toBeDisabled();
+  });
+
+  it('should show error state when refetch fails', async () => {
+    const { getRequestRejector } = await setup({
+      resolveRefreshManually: true,
+    });
+    const button = await screen.findByRole('button', { name: /Refresh/i });
+    await userEvent.click(button);
+    const refetchRejector = getRequestRejector();
+    refetchRejector();
+
+    expect(await screen.findByText('Error Panel')).toBeInTheDocument();
+    expect(screen.queryByText('Loading Skeleton')).not.toBeInTheDocument();
+  });
+  it('should display no stack trace when data.result is null or empty', async () => {
+    await setup({
+      resultStackTrace: '',
+    });
+    expect(screen.getByText('No stack trace...')).toBeInTheDocument();
+  });
+
+  it('should show spinner on refresh button when fetching', async () => {
+    await setup({
+      resolveRefreshManually: true,
+    });
+
+    const refreshButton = await screen.findByRole('button', {
+      name: /Refresh/i,
+    });
+    await userEvent.click(refreshButton);
+
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+  });
 });
 
 async function setup({
-  error,
-  wait,
+  initReqError,
+  resolveRefreshManually,
   resultStackTrace = 'test stack trace',
 }: {
-  error?: boolean;
-  wait?: boolean;
+  initReqError?: boolean;
+  resolveRefreshManually?: boolean;
+  resultStackTrace?: string;
+}) {
+  let requestResolver = () => {};
+  let requestRejector = () => {
+    console.log('rejector');
+  };
+  const getRequestResolver = () => requestResolver;
+  const getRequestRejector = () => requestRejector;
+  let requestIndex = -1;
+  render(
+    <Suspense fallback="Suspense placeholder">
+      <WorkflowStackTrace
+        params={{
+          domain: 'test-domain',
+          cluster: 'test-cluster',
+          runId: 'test-runid',
+          workflowId: 'test-workflowId',
+          workflowTab: 'stackTrace',
+        }}
+      />
+    </Suspense>,
+    {
+      endpointsMocks: [
+        {
+          path: '/api/domains/:domain/:cluster/workflows/:workflowId/:runId/query/__stack_trace',
+          httpMethod: 'POST',
+          mockOnce: false,
+          httpResolver: async () => {
+            requestIndex = requestIndex + 1;
+            if (requestIndex === 0 && initReqError)
+              return HttpResponse.json(
+                { message: 'Failed to fetch workflow stack trace' },
+                { status: 500 }
+              );
+
+            if (requestIndex > 0 && resolveRefreshManually) {
+              await new Promise((resolve, reject) => {
+                requestResolver = () =>
+                  resolve(
+                    HttpResponse.json(
+                      {
+                        rejected: null,
+                        result: resultStackTrace,
+                      } satisfies QueryWorkflowResponse,
+                      { status: 200 }
+                    )
+                  );
+                requestRejector = () =>
+                  reject(
+                    HttpResponse.json(
+                      { message: 'Failed to fetch workflow stack trace' },
+                      { status: 500 }
+                    )
+                  );
+              });
+            }
+            return HttpResponse.json(
+              {
+                rejected: null,
+                result: resultStackTrace,
+              } satisfies QueryWorkflowResponse,
+              { status: 200 }
+            );
+          },
+        },
+      ],
+    }
+  );
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryAllByText('Suspense placeholder')
+  );
+
+  return { getRequestResolver, getRequestRejector };
+}
+
+/* async function setup({
+  initReqError,
+  resolveRefreshManually,
+  resultStackTrace = 'test stack trace',
+}: {
+  initReqError?: boolean;
+  resolveRefreshManually?: boolean;
   resultStackTrace?: string;
 }) {
   let requestResolver = () => {};
   let requestRejector = () => {};
   const getRequestResolver = () => requestResolver;
   const getRequestRejector = () => requestRejector;
+  let requestIndex = -1;
   await act(async () => {
     render(
       <Suspense fallback="Suspense placeholder">
@@ -79,22 +214,19 @@ async function setup({
             httpMethod: 'POST',
             mockOnce: false,
             httpResolver: async () => {
-              if (wait) {
-                await new Promise<void>((resolve) => {
-                  requestResolver = () => {
-                    resolve();
-                  };
-                  requestRejector = () => {
-                    resolve();
-                  };
-                });
-              }
-              if (error)
+              requestIndex = requestIndex + 1;
+              if (requestIndex === 0 && initReqError)
                 return HttpResponse.json(
                   { message: 'Failed to fetch workflow stack trace' },
                   { status: 500 }
                 );
 
+              if (requestIndex > 0 && resolveRefreshManually) {
+                await new Promise<void>((resolve, reject) => {
+                  requestResolver = resolve;
+                  requestRejector = reject;
+                });
+              }
               return HttpResponse.json(
                 {
                   rejected: null,
@@ -110,4 +242,65 @@ async function setup({
   });
 
   return { getRequestResolver, getRequestRejector };
-}
+} */
+/* async function setup({
+  error,
+  wait,
+  resultStackTrace = 'test stack trace',
+}: {
+  error?: boolean;
+  wait?: boolean;
+  resultStackTrace?: string;
+}) {
+  let requestResolver = () => {};
+  const getRequestResolver = () => requestResolver;
+  render(
+    <Suspense fallback="Suspense placeholder">
+      <WorkflowStackTrace
+        params={{
+          domain: 'test-domain',
+          cluster: 'test-cluster',
+          runId: 'test-runid',
+          workflowId: 'test-workflowId',
+          workflowTab: 'stackTrace',
+        }}
+      />
+    </Suspense>,
+    {
+      endpointsMocks: [
+        {
+          path: '/api/domains/:domain/:cluster/workflows/:workflowId/:runId/query/__stack_trace',
+          httpMethod: 'POST',
+          mockOnce: false,
+          httpResolver: async () => {
+            if (wait) {
+              await new Promise<void>((resolve) => {
+                requestResolver = () => {
+                  resolve();
+                };
+              });
+            }
+            if (error)
+              return HttpResponse.json(
+                { message: 'Failed to fetch workflow history' },
+                { status: 500 }
+              );
+
+            return HttpResponse.json(
+              {
+                rejected: null,
+                result: resultStackTrace,
+              } satisfies QueryWorkflowResponse,
+              { status: 200 }
+            );
+          },
+        },
+      ],
+    }
+  );
+  await waitForElementToBeRemoved(() =>
+    screen.queryAllByText('Suspense placeholder')
+  );
+
+  return { getRequestResolver };
+} */
