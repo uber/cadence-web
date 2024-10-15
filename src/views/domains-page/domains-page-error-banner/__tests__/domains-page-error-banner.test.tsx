e2d027efa7aa11dee0f0d15fef1bd2e6238437ee
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-10-15 10:46:51 +0200

    Mock out error banner config

--- src/views/domains-page/domains-page-error-banner/__tests__/domains-page-error-banner.test.tsx
+++ src/views/domains-page/domains-page-error-banner/__tests__/domains-page-error-banner.test.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 import { render, screen } from '@/test-utils/rtl';
 
 import DomainsPageErrorBanner from '../domains-page-error-banner';
+import { type Props } from '../domains-page-error-banner.types';
+
+jest.mock('../../config/domains-page-error-banner.config', () => ({
+  icon: () => <div data-testid="mock-error-icon" />,
+  getErrorMessage: ({ failedClusters }: Props) =>
+    `Mock message for clusters failure: ${failedClusters.join(', ')}`,
+}));
 
 describe(DomainsPageErrorBanner.name, () => {
   it('should render if there are failed clusters', async () => {
@@ -10,9 +17,10 @@ describe(DomainsPageErrorBanner.name, () => {
       <DomainsPageErrorBanner failedClusters={['cluster_1', 'cluster_2']} />
     );
 
+    expect(await screen.findByTestId('mock-error-icon')).toBeInTheDocument();
     expect(
       await screen.findByText(
-        'Failed to fetch domains for following clusters: cluster_1, cluster_2'
+        'Mock message for clusters failure: cluster_1, cluster_2'
       )
     ).toBeInTheDocument();
   });
@@ -20,8 +28,7 @@ describe(DomainsPageErrorBanner.name, () => {
   it('should not render anything if no clusters failed', async () => {
     render(<DomainsPageErrorBanner failedClusters={[]} />);
 
-    expect(
-      screen.queryByText(/Failed to fetch domains for following clusters/)
-    ).toBeNull();
+    expect(screen.queryByTestId('mock-error-icon')).toBeNull();
+    expect(screen.queryByText(/Mock message for clusters failure/)).toBeNull();
   });
 });
