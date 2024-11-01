6aa074b541e73a9865f0ea410896bff11200c5dc
Assem Hafez <assem.a.hafez@gmail.com>
2024-11-01 13:54:39 +0100

    Disable prefetch by default (#708)
    * disable prefetch by default
    
    * fix lint

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -5,7 +5,12 @@ import NextLink from 'next/link';
 import { styled } from './link.styles';
 import { type Props } from './link.types';
 
-export default function Link({ href, children, ...restProps }: Props) {
+export default function Link({
+  href,
+  children,
+  prefetch = false,
+  ...restProps
+}: Props) {
   return (
     <styled.LinkBase {...restProps} $as={NextLink} href={href} disabled={!href}>
       {children}
