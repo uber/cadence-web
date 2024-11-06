ffffe495a7ac99d7661677c6cf79be29a0b3b5bd
Assem Hafez <assem.a.hafez@gmail.com>
2024-11-06 16:38:27 +0100

    Coerce fork event version (#713)
    * coerce fork event version
    
    * log parsing failurs
    
    * move the ignore statement

--- src/utils/data-formatters/format-workflow-history-event/__tests__/index.test.ts
+++ src/utils/data-formatters/format-workflow-history-event/__tests__/index.test.ts
@@ -1,11 +1,28 @@
+import { ZodError } from 'zod';
+
+import logger from '@/utils/logger';
 import { allWorkflowEvents } from '@/views/workflow-history/__fixtures__/all-workflow-event-types';
 
 import formatWorkflowHistoryEvent from '..';
+
+jest.mock('@/utils/logger');
 
 describe('formatWorkflowHistoryEvent', () => {
   allWorkflowEvents.forEach((event) => {
     it(`should format workflow ${event.attributes} to match snapshot`, () => {
       expect(formatWorkflowHistoryEvent(event)).toMatchSnapshot();
     });
+  });
+  it(`should log error if parsing failed`, () => {
+    expect(
+      //@ts-expect-error pass event with missing fields
+      formatWorkflowHistoryEvent({
+        attributes: 'workflowExecutionStartedEventAttributes',
+      })
+    ).toBe(null);
+    expect(logger.warn).toHaveBeenCalledWith(
+      { cause: expect.any(ZodError) },
+      'Failed to format workflow event'
+    );
   });
 });
