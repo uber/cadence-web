8943728e1d618f12fcbfd08ed5aeed906964a217
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-12-13 15:01:24 +0530

    Fixes to List Workflows Basic route handler (#758)
    Add extra validation to Zod schema to ensure that only one filter is set
    Explicitly pass filter type to GRPC endpoint
    Rewrite the endpoint logic a bit to fix type errors

--- src/route-handlers/list-workflows-basic/schemas/list-workflows-basic-query-params-schema.ts
+++ src/route-handlers/list-workflows-basic/schemas/list-workflows-basic-query-params-schema.ts
@@ -4,25 +4,44 @@ import getGrpcTimestampFromIso from '@/utils/datetime/get-grpc-timestamp-from-is
 import isWorkflowStatus from '@/views/shared/workflow-status-tag/helpers/is-workflow-status';
 import { type WorkflowStatus } from '@/views/shared/workflow-status-tag/workflow-status-tag.types';
 
-const listWorkflowsBasicQueryParamsSchema = z.object({
-  kind: z.enum(['open', 'closed']),
-  pageSize: z
-    .string()
-    .transform((val) => parseInt(val, 10))
-    .pipe(
-      z.number().positive({ message: 'Page size must be a positive integer' })
-    ),
-  workflowId: z.string().optional(),
-  workflowType: z.string().optional(),
-  runId: z.string().optional(),
-  closeStatus: z
-    .custom<WorkflowStatus>(isWorkflowStatus, {
-      message: 'Invalid workflow status',
-    })
-    .optional(),
-  timeRangeStart: z.string().datetime().transform(getGrpcTimestampFromIso),
-  timeRangeEnd: z.string().datetime().transform(getGrpcTimestampFromIso),
-  nextPage: z.string().optional(),
-});
+const listWorkflowsBasicQueryParamsSchema = z
+  .object({
+    kind: z.enum(['open', 'closed']),
+    pageSize: z
+      .string()
+      .transform((val) => parseInt(val, 10))
+      .pipe(
+        z.number().positive({ message: 'Page size must be a positive integer' })
+      ),
+    workflowId: z.string().optional(),
+    workflowType: z.string().optional(),
+    runId: z.string().optional(),
+    closeStatus: z
+      .custom<WorkflowStatus>(isWorkflowStatus, {
+        message: 'Invalid workflow status',
+      })
+      .optional(),
+    timeRangeStart: z.string().datetime().transform(getGrpcTimestampFromIso),
+    timeRangeEnd: z.string().datetime().transform(getGrpcTimestampFromIso),
+    nextPage: z.string().optional(),
+  })
+  .refine((params) => {
+    let definedFiltersCount = 0;
+    // Execution filter
+    if (params.workflowId || params.runId) {
+      definedFiltersCount += 1;
+    }
+    // Type filter
+    if (params.workflowType) {
+      definedFiltersCount += 1;
+    }
+    // Status filter
+    if (params.closeStatus) {
+      definedFiltersCount += 1;
+    }
+
+    return definedFiltersCount <= 1;
+    // TODO @adhitya.mamallan - If we add RunID back, add it in the message below
+  }, 'Only one of the following filters is allowed - Workflow ID, Workflow Type, Close Status');
 
 export default listWorkflowsBasicQueryParamsSchema;
